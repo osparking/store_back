0f003e2f73c33588f7f05cf0f1dc122b55684e85
osparking <jbpark03@gmail.com>
2025-02-25 20:41:37 +0900

    use annot on Admin

--- src/main/java/com/bumsoap/store/model/Admin.java
+++ src/main/java/com/bumsoap/store/model/Admin.java
@@ -1,4 +1,17 @@
 package com.bumsoap.store.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@PrimaryKeyJoinColumn(name = "admin_id")
 public class Admin extends Employee {
+    private long id;
 }
