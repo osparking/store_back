8482adf40dd690a38eeb7a25170c2c99021242d9
osparking <jbpark03@gmail.com>
2025-02-25 20:35:41 +0900

    use annot on BsUser

--- src/main/java/com/bumsoap/store/model/BsUser.java
+++ src/main/java/com/bumsoap/store/model/BsUser.java
@@ -1,10 +1,23 @@
 package com.bumsoap.store.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * BumSoap(범이비누) 사용자
  * @author <a href="mailto:jbpark03@gmail.com">박종범
  */
+@Getter @Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
 public class BsUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String fullName;
     private String mbPhone; // Mobile Phone
