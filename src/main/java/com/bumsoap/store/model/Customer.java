0d90ff3a6ca336f235acf70a1d12b38bced855b5
osparking <jbpark03@gmail.com>
2025-02-25 20:58:16 +0900

    use annot on Customer

--- src/main/java/com/bumsoap/store/model/Customer.java
+++ src/main/java/com/bumsoap/store/model/Customer.java
@@ -1,4 +1,18 @@
 package com.bumsoap.store.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@PrimaryKeyJoinColumn(name = "customer_id")
 public class Customer extends BsUser{
+    private long id;
 }
