84e09a8124a973f39f6bb1de9ed14daa2392b606
osparking <jbpark03@gmail.com>
2025-02-25 20:56:40 +0900

    use annot on Employee

--- src/main/java/com/bumsoap/store/model/Employee.java
+++ src/main/java/com/bumsoap/store/model/Employee.java
@@ -1,5 +1,19 @@
 package com.bumsoap.store.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@PrimaryKeyJoinColumn(name = "employee_id")
 public class Employee extends BsUser{
-    Photo photo;
+    private long id;
+    private Photo photo;
 }
