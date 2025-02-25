d49ae682ac3879f473fc01fe2c77173a46fcf2dd
osparking <jbpark03@gmail.com>
2025-02-25 17:55:41 +0900

    bumsoap user class - w/ only 6 members

---
+++ src/main/java/com/bumsoap/store/model/BsUser.java
@@ -0,0 +1,14 @@
+package com.bumsoap.store.model;
+
+/**
+ * BumSoap(범이비누) 사용자
+ * @author <a href="mailto:jbpark03@gmail.com">박종범
+ */
+public class BsUser {
+    private long id;
+    private String fullName;
+    private String mbPhone; // Mobile Phone
+    private String email;
+    private String password;
+    boolean usable;
+}
