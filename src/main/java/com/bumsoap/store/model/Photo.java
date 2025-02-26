d888d369202f724f9858cba85306cca268f1b74d
osparking <jbpark03@gmail.com>
2025-02-26 10:02:45 +0900

    add annot' to Photo entity class

--- src/main/java/com/bumsoap/store/model/Photo.java
+++ src/main/java/com/bumsoap/store/model/Photo.java
@@ -1,10 +1,22 @@
 package com.bumsoap.store.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.sql.Blob;
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Photo {
-    long id;
-    String fileType;
-    String fileName;
-    Blob image;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String fileType;
+    private String fileName;
+    @Lob
+    private Blob image;
 }
