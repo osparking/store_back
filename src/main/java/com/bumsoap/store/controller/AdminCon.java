8e3e12b72d362b91fbf2195ebaba96364cea660e
osparking <jbpark03@gmail.com>
2025-02-26 15:32:00 +0900

    add annot to admin add controller param

--- src/main/java/com/bumsoap/store/controller/AdminCon.java
+++ src/main/java/com/bumsoap/store/controller/AdminCon.java
@@ -4,6 +4,7 @@ import com.bumsoap.store.model.Admin;
 import com.bumsoap.store.service.AdminServ;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ public class AdminCon {
     private final AdminServ adminServ;
 
     @PostMapping
-    public void add(Admin admin) {
+    public void add(@RequestBody Admin admin) {
         adminServ.add(admin);
     }
 }
