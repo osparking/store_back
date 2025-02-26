08d730b379b43625e363ea0c2dbb5f9e65d34692
osparking <jbpark03@gmail.com>
2025-02-26 14:31:19 +0900

    add annot' and define add controller

--- src/main/java/com/bumsoap/store/controller/AdminCon.java
+++ src/main/java/com/bumsoap/store/controller/AdminCon.java
@@ -1,4 +1,20 @@
 package com.bumsoap.store.controller;
 
+import com.bumsoap.store.model.Admin;
+import com.bumsoap.store.service.AdminServ;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bs_users")
+@RequiredArgsConstructor
 public class AdminCon {
+    private final AdminServ adminServ;
+
+    @PostMapping
+    public void add(Admin admin) {
+        adminServ.add(admin);
+    }
 }
