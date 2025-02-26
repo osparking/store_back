69b7d8ad0b025f5a00b7e554ca15261345f7a43c
osparking <jbpark03@gmail.com>
2025-02-26 14:26:53 +0900

    add annot' and define add service

--- src/main/java/com/bumsoap/store/service/AdminServ.java
+++ src/main/java/com/bumsoap/store/service/AdminServ.java
@@ -1,4 +1,15 @@
 package com.bumsoap.store.service;
 
+import com.bumsoap.store.model.Admin;
+import com.bumsoap.store.repository.AdminRepoI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AdminServ {
+    private final AdminRepoI adminRepo;
+    public void add(Admin admin) {
+        adminRepo.save(admin);
+    }
 }
