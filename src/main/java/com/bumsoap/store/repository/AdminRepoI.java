592dd0b5331d3bf63fb39ed83952cb047438ff3d
osparking <jbpark03@gmail.com>
2025-02-26 14:22:56 +0900

    set super interface and add annot'

--- src/main/java/com/bumsoap/store/repository/AdminRepoI.java
+++ src/main/java/com/bumsoap/store/repository/AdminRepoI.java
@@ -1,4 +1,9 @@
 package com.bumsoap.store.repository;
 
-public interface AdminRepoI {
+import com.bumsoap.store.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepoI extends JpaRepository<Admin, Long> {
 }
