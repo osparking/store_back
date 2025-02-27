ef7f9acebd785eade6043bf3d7b215224795550f
osparking <jbpark03@gmail.com>
2025-02-27 12:26:25 +0900

    expand userCon.add for customer and worker

--- src/main/java/com/bumsoap/store/controller/UserCon.java
+++ src/main/java/com/bumsoap/store/controller/UserCon.java
@@ -2,12 +2,13 @@ package com.bumsoap.store.controller;
 
 import com.bumsoap.store.dto.ObjMapper;
 import com.bumsoap.store.model.Admin;
+import com.bumsoap.store.model.Customer;
+import com.bumsoap.store.model.Worker;
 import com.bumsoap.store.request.UserRegisterReq;
 import com.bumsoap.store.service.AdminServ;
 import com.bumsoap.store.service.CustomerServ;
 import com.bumsoap.store.service.WorkerServ;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserCon {
     private final ObjMapper objMapper;
     private final AdminServ adminServ;
+    private final CustomerServ customerServ;
+    private final WorkerServ workerServ;
 
     @PostMapping("/add")
     public void add(@RequestBody UserRegisterReq request) {
@@ -26,6 +29,14 @@ public class UserCon {
             case "ADMIN":
                 var admin = objMapper.mapToDto(request, Admin.class);
                 adminServ.add(admin);
+                break;
+            case "CUSTOMER":
+                var customer = objMapper.mapToDto(request, Customer.class);
+                customerServ.add(customer);
+                break;
+            case "WORKER":
+                var worker = objMapper.mapToDto(request, Worker.class);
+                workerServ.add(worker);
                 break;
             default:
                 throw new IllegalArgumentException("존재하지 않는 유저 유형");
