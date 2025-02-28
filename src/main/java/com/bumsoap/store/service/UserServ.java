b7ee29911653912f9527f64cbcc6f9ac09b13834
osparking <jbpark03@gmail.com>
2025-02-28 12:44:45 +0900

    introduce interface for user service class

--- src/main/java/com/bumsoap/store/service/UserServ.java
+++ src/main/java/com/bumsoap/store/service/UserServ.java
@@ -2,6 +2,7 @@ package com.bumsoap.store.service;
 
 import com.bumsoap.store.model.BsUser;
 import com.bumsoap.store.repository.UserRepoI;
+import com.bumsoap.store.service.user.UserServInt;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +10,9 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class UserServ {
+public class UserServ implements UserServInt {
     private final UserRepoI userRepo;
+    @Override
     public BsUser getUserById(Long id) {
         Optional<BsUser> userOptional = userRepo.findById(id);
         if (userOptional.isPresent()) {
