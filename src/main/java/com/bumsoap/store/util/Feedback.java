4834b760366c2c679542967373c2446c62b95583
osparking <jbpark03@gmail.com>
2025-02-28 10:22:34 +0900

    feedback messages gathered in a file

---
+++ src/main/java/com/bumsoap/store/util/Feedback.java
@@ -0,0 +1,8 @@
+package com.bumsoap.store.util;
+
+public class Feedback {
+    /********************** 유저 처리 관련 메시지 ********************/
+    public static final String USER_ADD_SUCCESS = "유저 등록 성공";
+    public static final String USER_TYPE_WRONG = "존재하지 않는 유저 유형";
+    public static final String USER_TAKEN_EMAIL = "이미 사용 중인 이메일:";
+}
