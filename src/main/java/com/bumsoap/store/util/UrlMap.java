df0aee31443975d2b9fe77307d34ce296ce65e37
osparking <jbpark03@gmail.com>
2025-02-28 11:33:37 +0900

    define URL strings in combined way

---
+++ src/main/java/com/bumsoap/store/util/UrlMap.java
@@ -0,0 +1,14 @@
+package com.bumsoap.store.util;
+
+public class UrlMap {
+    public static final String API = "/api/s1";
+
+    /*== Common / 공통 시작 ==================================================*/
+    public static final String ADD = "/add";
+    public static final String GET_BY_ID = "/{id}/get";
+    /*== Common / 공통 끝 ===============================*/
+
+    /*== USER / 유저 시작 ====================================================*/
+    public static final String USER = API + "/user";
+    /*== USER / 유저 끝 ==================================*/
+}
