81343d11076fbf5ea8dca0330090266bfb2e0aa8
osparking <jbpark03@gmail.com>
2025-03-01 20:09:45 +0900

    service interface for photo management

---
+++ src/main/java/com/bumsoap/store/service/photo/PhotoInt.java
@@ -0,0 +1,14 @@
+package com.bumsoap.store.service.photo;
+
+import com.bumsoap.store.model.Photo;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
+public interface PhotoInt {
+    Photo save(Long userId, MultipartFile photo);
+    Optional<Photo> findById(Long id);
+    void deleteById(Long id);
+    Photo update(Long id, byte[] imageData);
+    byte[] getImageData(Long id);
+}
