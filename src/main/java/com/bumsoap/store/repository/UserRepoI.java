3e1387401ff00f747037ce098ad124f54176b195
osparking <jbpark03@gmail.com>
2025-03-02 22:13:38 +0900

    reads all user related data from DB

--- src/main/java/com/bumsoap/store/repository/UserRepoI.java
+++ src/main/java/com/bumsoap/store/repository/UserRepoI.java
@@ -1,10 +1,23 @@
 package com.bumsoap.store.repository;
 
+import com.bumsoap.store.dto.UserDto;
 import com.bumsoap.store.model.BsUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepoI extends JpaRepository<BsUser, Long> {
     boolean existsByEmail(String email);
+
+    @Query(nativeQuery = true,
+           value = "select u.id, u.full_name, u.mb_phone, u.email, " +
+                   "u.usable, w.dept, u.add_date, e.photo_id, p.image " +
+                   "from bs_user u " +
+                   "left join employee e on e.employee_id = u.id " +
+                   "left join worker w on w.worker_id = e.employee_id " +
+                   "left join photo p on p.id = e.photo_id ")
+    Optional<UserDto> findUserDtoById(long id);
 }
