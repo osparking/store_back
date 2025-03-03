848cbc01832b02157579b1aab058b4e34372817b
osparking <jbpark03@gmail.com>
2025-03-03 12:21:40 +0900

    read repo for all user dto list

--- src/main/java/com/bumsoap/store/repository/UserRepoI.java
+++ src/main/java/com/bumsoap/store/repository/UserRepoI.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -22,4 +23,13 @@ public interface UserRepoI extends JpaRepository<BsUser, Long> {
                    "left join photo p on p.id = e.photo_id " +
                    "where u.id = :id")
     Optional<UserDto> findUserDtoById(@Param("id")long id);
+
+    @Query(nativeQuery = true,
+           value = "select u.id, u.full_name, u.mb_phone, u.email, " +
+                   "u.usable, w.dept, u.add_date, e.photo_id, p.image " +
+                   "from bs_user u " +
+                   "left join employee e on e.employee_id = u.id " +
+                   "left join worker w on w.worker_id = e.employee_id " +
+                   "left join photo p on p.id = e.photo_id")
+    List<UserDto> findAllUserDto();
 }
