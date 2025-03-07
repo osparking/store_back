fffe131ec1cc07e669e6b7829c4118474d5dab29
osparking <jbpark03@gmail.com>
2025-03-08 08:17:37 +0900

    useless hander method removed for WorkerCon

--- src/main/java/com/bumsoap/store/controller/WorkerCon.java
+++ src/main/java/com/bumsoap/store/controller/WorkerCon.java
@@ -1,13 +1,15 @@
 package com.bumsoap.store.controller;
 
-import com.bumsoap.store.model.Worker;
 import com.bumsoap.store.response.ApiResp;
 import com.bumsoap.store.service.worker.WorkerServInt;
 import com.bumsoap.store.util.Feedback;
 import com.bumsoap.store.util.UrlMap;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(UrlMap.WORKER)
@@ -15,11 +17,6 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin("http://localhost:5173/")
 public class WorkerCon {
     private final WorkerServInt workerServ;
-
-    @PostMapping("/add")
-    public void add(@RequestBody Worker worker) {
-        workerServ.add(worker);
-    }
 
     @GetMapping(UrlMap.GET_ALL_DEPT)
     public ResponseEntity<ApiResp> findAllDept() {
