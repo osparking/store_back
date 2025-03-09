6c65ef91b18f9c8d40daef659cef4d02d5afb7f0
osparking <jbpark03@gmail.com>
2025-03-10 07:12:07 +0900

    select query string extracted and reused

--- src/main/java/com/bumsoap/store/repository/UserRepoI.java
+++ src/main/java/com/bumsoap/store/repository/UserRepoI.java
@@ -12,24 +12,22 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepoI extends JpaRepository<BsUser, Long> {
+
     boolean existsByEmail(String email);
 
-    @Query(nativeQuery = true,
-           value = "select u.id, u.full_name, u.mb_phone, u.email, u.usable, " +
-                   "w.dept, u.user_type, u.add_date, e.photo_id, p.image " +
-                   "from bs_user u " +
-                   "left join employee e on e.employee_id = u.id " +
-                   "left join worker w on w.worker_id = e.employee_id " +
-                   "left join photo p on p.id = e.photo_id " +
-                   "where u.id = :id")
-    Optional<UserDto> findUserDtoById(@Param("id")long id);
+    String selectUserDto =
+            "select u.id, u.full_name, u.mb_phone, u.email, u.usable," +
+            " w.dept, u.user_type, u.add_date, e.photo_id, p.image " +
+            "from bs_user u" +
+            " left join employee e on e.employee_id = u.id" +
+            " left join worker w on w.worker_id = e.employee_id" +
+            " left join photo p on p.id = e.photo_id";
+    String selectUserDtoById = selectUserDto + " where u.id = :id";
 
     @Query(nativeQuery = true,
-           value = "select u.id, u.full_name, u.mb_phone, u.email, u.usable, " +
-                   "w.dept, u.user_type, u.add_date, e.photo_id, p.image " +
-                   "from bs_user u " +
-                   "left join employee e on e.employee_id = u.id " +
-                   "left join worker w on w.worker_id = e.employee_id " +
-                   "left join photo p on p.id = e.photo_id")
+           value = selectUserDtoById)
+    Optional<UserDto> findUserDtoById(@Param("id")long id);
+
+    @Query(nativeQuery = true, value = selectUserDto)
     List<UserDto> findAllUserDto();
 }
