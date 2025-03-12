b05d42af3fa7cc82c5e8d37285366c73a4f2a552
osparking <jbpark03@gmail.com>
2025-03-12 21:27:26 +0900

    user token generator - part 1

--- src/main/java/com/bumsoap/store/security/jwt/JwtUtilBean.java
+++ src/main/java/com/bumsoap/store/security/jwt/JwtUtilBean.java
@@ -1,6 +1,21 @@
 package com.bumsoap.store.security.jwt;
 
+import com.bumsoap.store.security.user.BsUserDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.List;
+
 public class JwtUtilBean {
     private String jwtSecret;
     private int expirationMs;
+
+    public String generateTokenForUser(Authentication authentication) {
+        var userDetails = (BsUserDetails) authentication.getPrincipal();
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority).toList();
+
+        return "";
+    }
 }
