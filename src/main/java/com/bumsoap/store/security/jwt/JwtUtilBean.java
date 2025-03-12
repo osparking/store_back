77c5c1325685c4dac13a820fbbf7984433063858
osparking <jbpark03@gmail.com>
2025-03-13 06:26:38 +0900

    user token key generator method

--- src/main/java/com/bumsoap/store/security/jwt/JwtUtilBean.java
+++ src/main/java/com/bumsoap/store/security/jwt/JwtUtilBean.java
@@ -1,14 +1,21 @@
 package com.bumsoap.store.security.jwt;
 
 import com.bumsoap.store.security.user.BsUserDetails;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 
+import java.security.Key;
 import java.util.List;
 
 public class JwtUtilBean {
     private String jwtSecret;
     private int expirationMs;
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
 
     public String generateTokenForUser(Authentication authentication) {
         var userDetails = (BsUserDetails) authentication.getPrincipal();
