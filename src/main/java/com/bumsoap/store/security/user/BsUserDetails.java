228935293355802ed66728da849d8b5c505149d0
osparking <jbpark03@gmail.com>
2025-03-12 19:20:21 +0900

    let BsUserDetails implements UserDetails

--- src/main/java/com/bumsoap/store/security/user/BsUserDetails.java
+++ src/main/java/com/bumsoap/store/security/user/BsUserDetails.java
@@ -1,14 +1,31 @@
 package com.bumsoap.store.security.user;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
-public class BsUserDetails {
+public class BsUserDetails implements UserDetails {
     private Long id;
     private String email;
     private String password;
     private boolean usable;
 
     private Collection<GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
 }
