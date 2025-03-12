c6f487a24ada25de825e3019c5c2af48f2e38531
osparking <jbpark03@gmail.com>
2025-03-12 19:29:47 +0900

    bsUserDetails factory method

--- src/main/java/com/bumsoap/store/security/user/BsUserDetails.java
+++ src/main/java/com/bumsoap/store/security/user/BsUserDetails.java
@@ -1,11 +1,20 @@
 package com.bumsoap.store.security.user;
 
+import com.bumsoap.store.model.BsUser;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BsUserDetails implements UserDetails {
     private Long id;
     private String email;
@@ -13,6 +22,15 @@ public class BsUserDetails implements UserDetails {
     private boolean usable;
 
     private Collection<GrantedAuthority> authorities;
+
+    public static BsUserDetails buildUserDetails(BsUser user) {
+        List<GrantedAuthority> authorities = user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+        return new BsUserDetails(user.getId(), user.getEmail(),
+                user.getPassword(), user.isUsable(), authorities);
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
