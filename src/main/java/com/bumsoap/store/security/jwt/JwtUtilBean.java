451c80b6a95687e0123a3bc01cf1dc3ecc6ca2eb
osparking <jbpark03@gmail.com>
2025-03-13 07:06:01 +0900

    check if token is valid using key

--- src/main/java/com/bumsoap/store/security/jwt/JwtUtilBean.java
+++ src/main/java/com/bumsoap/store/security/jwt/JwtUtilBean.java
@@ -1,22 +1,34 @@
 package com.bumsoap.store.security.jwt;
 
 import com.bumsoap.store.security.user.BsUserDetails;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.List;
 
+@Component
 public class JwtUtilBean {
     private String jwtSecret;
     private int expirationMs;
 
     private Key key() {
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(key()).build().parse(token);
+            return true;
+        } catch (Exception e) {
+            throw new JwtException(e.getMessage());
+        }
     }
 
     public String getUsernameFrom(String token) {
