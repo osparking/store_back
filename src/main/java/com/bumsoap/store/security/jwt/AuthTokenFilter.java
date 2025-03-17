61a0c791b47477e3e53fbaf87449425e957c9174
박종범 <jbpark03@gmail.com>
2025-03-17 20:04:46 +0900

    authen' token filter class - 1st half

---
+++ src/main/java/com/bumsoap/store/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,27 @@
+package com.bumsoap.store.security.jwt;
+
+import com.bumsoap.store.security.user.BsUserDetails;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthTokenFilter extends OncePerRequestFilter {
+    private JwtUtilBean jwtUtilBean;
+    private BsUserDetails bsUserDetails;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+    }
+}
