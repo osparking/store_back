331f0eadb218536bafbcf3b3fe116f443e947192
박종범 <jbpark03@gmail.com>
2025-03-17 22:18:59 +0900

    using email, w/ help of service create authen'

--- src/main/java/com/bumsoap/store/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/bumsoap/store/security/jwt/AuthTokenFilter.java
@@ -1,12 +1,14 @@
 package com.bumsoap.store.security.jwt;
 
 import com.bumsoap.store.security.user.BsUserDetails;
+import com.bumsoap.store.security.user.BsUserDetailsService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -15,7 +17,7 @@ import java.io.IOException;
 @NoArgsConstructor
 public class AuthTokenFilter extends OncePerRequestFilter {
     private JwtUtilBean jwtUtilBean;
-    private BsUserDetails bsUserDetails;
+    private BsUserDetailsService bsUserDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
@@ -26,6 +28,9 @@ public class AuthTokenFilter extends OncePerRequestFilter {
             String jwt = getJwtFromRequest(request);
             if (jwt != null && jwtUtilBean.validateToken(jwt)) {
                 String email = jwtUtilBean.getUsernameFrom(jwt);
+                var details = bsUserDetailsService.loadUserByUsername(email);
+                var authentication = new UsernamePasswordAuthenticationToken(
+                        details, details.getAuthorities());
             }
         } catch (Exception e) { }
     }
