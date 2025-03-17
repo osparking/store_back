aef9ca6c851bcf3c20d1e4ef638351a3a51546a6
박종범 <jbpark03@gmail.com>
2025-03-18 06:48:28 +0900

    add nonnull anno' to doFilterInternal params

--- src/main/java/com/bumsoap/store/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/bumsoap/store/security/jwt/AuthTokenFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -22,9 +23,9 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     private BsUserDetailsService bsUserDetailsService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
             throws ServletException, IOException {
         try {
             String jwt = getJwtFromRequest(request);
