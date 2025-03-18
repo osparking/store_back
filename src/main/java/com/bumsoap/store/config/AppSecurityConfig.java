226a0e020ac665f4233d188950eeb790e451d23b
박종범 <jbpark03@gmail.com>
2025-03-18 10:45:29 +0900

    desirable password encoder bean method

--- src/main/java/com/bumsoap/store/config/AppSecurityConfig.java
+++ src/main/java/com/bumsoap/store/config/AppSecurityConfig.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -17,5 +19,9 @@ public class AppSecurityConfig {
     @Bean
     public AuthTokenFilter authTokenFilter() {
         return new AuthTokenFilter();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
