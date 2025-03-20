8b73ccd6a5e191d47dd531ce82dc14d8ae36812b
박종범 <jbpark03@gmail.com>
2025-03-20 21:57:37 +0900

    JWT response class

---
+++ src/main/java/com/bumsoap/store/response/JwtResponse.java
@@ -0,0 +1,13 @@
+package com.bumsoap.store.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class JwtResponse {
+    private Long id;
+    private String token;
+}
