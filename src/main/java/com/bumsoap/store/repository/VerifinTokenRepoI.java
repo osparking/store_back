91c174549f2043f6bba40f1ec2fa70aa707f40c8
박종범 <jbpark03@gmail.com>
2025-03-21 21:08:24 +0900

    verification token repository interface

---
+++ src/main/java/com/bumsoap/store/repository/VerifinTokenRepoI.java
@@ -0,0 +1,7 @@
+package com.bumsoap.store.repository;
+
+import com.bumsoap.store.model.VerifinToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerifinTokenRepoI extends JpaRepository<VerifinToken, Long> {
+}
