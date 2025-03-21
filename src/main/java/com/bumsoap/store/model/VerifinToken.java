657fce33a08b2b69b0a663766a3949183bd816b7
박종범 <jbpark03@gmail.com>
2025-03-21 18:06:51 +0900

    creates a verifin token that expires in 10 min

--- src/main/java/com/bumsoap/store/model/VerifinToken.java
+++ src/main/java/com/bumsoap/store/model/VerifinToken.java
@@ -1,5 +1,6 @@
 package com.bumsoap.store.model;
 
+import com.bumsoap.store.util.BsUtils;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,4 +19,10 @@ public class VerifinToken {
     @ManyToOne
     @JoinColumn(name="user_id")
     private BsUser user;
+
+    public VerifinToken(String token, BsUser user) {
+        this.token = token;
+        this.expireDate = BsUtils.getExpireTime();
+        this.user = user;
+    }
 }
