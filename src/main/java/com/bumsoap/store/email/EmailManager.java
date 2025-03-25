0653fee5d862e252df7f4bb0e66dbe7971a2fe49
Park, Jongbum <jbpark03@gmail.com>
2025-03-25 19:53:19 +0900

    creates a mail sender object

--- src/main/java/com/bumsoap/store/email/EmailManager.java
+++ src/main/java/com/bumsoap/store/email/EmailManager.java
@@ -3,10 +3,12 @@ package com.bumsoap.store.email;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Properties;
 
 @Component
 public class EmailManager {
@@ -22,5 +24,17 @@ public class EmailManager {
         messageHelper.setSubject(subject);
         messageHelper.setText(mailContent, true);
         sender.send(message);
+    }
+
+    private JavaMailSender createMailSender() {
+        JavaMailSenderImpl mailSenderImpl = new JavaMailSenderImpl();
+        mailSenderImpl.setHost(EmailProperties.DEFAULT_HOST);
+        mailSenderImpl.setPort(EmailProperties.DEFAULT_PORT);
+        mailSenderImpl.setUsername(EmailProperties.DEFAULT_USERNAME);
+        mailSenderImpl.setPassword(EmailProperties.DEFAULT_PASSWORD);
+        Properties props = mailSenderImpl.getJavaMailProperties();
+        props.put("mail.smtp.auth", EmailProperties.DEFAULT_AUTH);
+        props.put("mail.smtp.starttls.enable", EmailProperties.DEFAULT_STARTTLS);
+        return mailSenderImpl;
     }
 }
