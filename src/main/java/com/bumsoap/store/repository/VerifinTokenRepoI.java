fa99d25791dc6e15636c0798d9cbe30ad6a18109
Park, Jongbum <jbpark03@gmail.com>
2025-03-25 11:58:35 +0900

    delete verifin token with given condition

--- src/main/java/com/bumsoap/store/repository/VerifinTokenRepoI.java
+++ src/main/java/com/bumsoap/store/repository/VerifinTokenRepoI.java
@@ -2,9 +2,15 @@ package com.bumsoap.store.repository;
 
 import com.bumsoap.store.model.VerifinToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface VerifinTokenRepoI extends JpaRepository<VerifinToken, Long> {
     Optional<VerifinToken> findByToken(String token);
+
+    @Query(value = "DELETE FROM verifin_token WHERE id != :tokenId AND user_id = :userId",
+            nativeQuery = true)
+    void deleteByUserId(@Param("tokenId") Long tokenId, @Param("userId") Long userId);
 }
