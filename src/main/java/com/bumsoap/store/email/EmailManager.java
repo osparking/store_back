71fb63313d1a737dc15071ffb7a069b273952504
Park, Jongbum <jbpark03@gmail.com>
2025-03-25 18:10:00 +0900

    sends mail using passed params

--- src/main/java/com/bumsoap/store/email/EmailManager.java
+++ src/main/java/com/bumsoap/store/email/EmailManager.java
@@ -1,14 +1,26 @@
 package com.bumsoap.store.email;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+
+import java.io.UnsupportedEncodingException;
 
 @Component
 public class EmailManager {
     private JavaMailSender sender;
 
     public void sendMail(String to, String subject, String senderName,
-                         String mailContent) {
-
+                         String mailContent)
+            throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = sender.createMimeMessage();
+        var messageHelper = new MimeMessageHelper(message);
+        messageHelper.setFrom(EmailProperties.DEFAULT_USERNAME, senderName);
+        messageHelper.setTo(to);
+        messageHelper.setSubject(subject);
+        messageHelper.setText(mailContent, true);
+        sender.send(message);
     }
 }
