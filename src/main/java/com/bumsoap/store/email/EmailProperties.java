73e09849eec9a35ec04a04eaa6c886e676394a4d
박종범 <jbpark03@gmail.com>
2025-03-27 10:42:03 +0900

    capitalize gmail app pwd env var name

--- src/main/java/com/bumsoap/store/email/EmailProperties.java
+++ src/main/java/com/bumsoap/store/email/EmailProperties.java
@@ -1,12 +1,15 @@
 package com.bumsoap.store.email;
 
+import lombok.Data;
+
+//@Data
 public class EmailProperties {
     public static final String DEFAULT_HOST = "smtp.gmail.com";
     public static final int DEFAULT_PORT = 587;
     public static final String DEFAULT_SENDER = "jbpark03@gmail.com";
     public static final String DEFAULT_USERNAME = "jbpark03@gmail.com";
     public static final String DEFAULT_PASSWORD
-            = System.getenv("bumsoap_app_pwd");
+            = System.getenv("GMAIL_APP_PWD");
     public static final boolean DEFAULT_AUTH = true;
     public static final boolean DEFAULT_STARTTLS = true;
 }
