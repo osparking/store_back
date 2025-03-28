c967c9753538d0261d6fc7224868c7934fcd2a79
박종범 <jbpark03@gmail.com>
2025-03-29 08:27:44 +0900

    make sure 3 default roles exist in Role table

--- src/main/java/com/bumsoap/store/data/InitialUserCreator.java
+++ src/main/java/com/bumsoap/store/data/InitialUserCreator.java
@@ -1,5 +1,7 @@
 package com.bumsoap.store.data;
 
+import com.bumsoap.store.model.Role;
+import com.bumsoap.store.repository.RoleRepoI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
@@ -10,8 +12,17 @@ import java.util.Set;
 @Component
 @RequiredArgsConstructor
 public class InitialUserCreator implements ApplicationListener<ApplicationReadyEvent> {
+    private final RoleRepoI roleRepo;
+
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
         Set<String> defaultRoles = Set.of("ROLE_ADMIN", "ROLE_CUSTOMER", "ROLE_WORKER");
+        insertRolesIfNotExits(defaultRoles);
+    }
+
+    private void insertRolesIfNotExits(Set<String> roles) {
+        roles.stream()
+                .filter(role -> roleRepo.findByName(role).isEmpty())
+                .map(Role::new).forEach(roleRepo::save);
     }
 }
