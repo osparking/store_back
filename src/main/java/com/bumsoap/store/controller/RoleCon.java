3d4c7a37896b8c04160d86e715377b4963a824ac
박종범 <jbpark03@gmail.com>
2025-03-28 22:03:24 +0900

    get all roles endpoint

--- src/main/java/com/bumsoap/store/controller/RoleCon.java
+++ src/main/java/com/bumsoap/store/controller/RoleCon.java
@@ -1,14 +1,23 @@
 package com.bumsoap.store.controller;
 
+import com.bumsoap.store.model.Role;
 import com.bumsoap.store.service.role.RoleServInt;
 import com.bumsoap.store.util.UrlMap;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 @RequestMapping(UrlMap.ROLE)
 public class RoleCon {
     private final RoleServInt roleServ;
+
+    @GetMapping(UrlMap.GET_ALL)
+    public List<Role> getAllRole() {
+        return roleServ.getRoles();
+    }
 }
