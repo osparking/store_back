a8b68725872f34da87579b5f40c5ce9e09ef4b72
박종범 <jbpark03@gmail.com>
2025-03-28 14:12:00 +0900

    let spring MVC handle CORS header for response

--- src/main/java/com/bumsoap/store/config/AppSecurityConfig.java
+++ src/main/java/com/bumsoap/store/config/AppSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -48,6 +49,7 @@ public class AppSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .exceptionHandling(exception
                         -> exception.authenticationEntryPoint(bsJwtErrorEntry))
                 .sessionManagement(session
