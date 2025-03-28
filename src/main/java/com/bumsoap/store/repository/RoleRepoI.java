08b4545ee8c06bb94b70c1e0ebc1509a3cd53be3
박종범 <jbpark03@gmail.com>
2025-03-28 20:17:45 +0900

    finds Role by name if exists

--- src/main/java/com/bumsoap/store/repository/RoleRepoI.java
+++ src/main/java/com/bumsoap/store/repository/RoleRepoI.java
@@ -3,5 +3,8 @@ package com.bumsoap.store.repository;
 import com.bumsoap.store.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepoI extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
 }
