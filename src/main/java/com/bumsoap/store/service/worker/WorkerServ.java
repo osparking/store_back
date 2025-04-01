112e11bffac60f04ea935aee3765aff561d46f68
박종범 <jbpark03@gmail.com>
2025-04-01 15:23:29 +0900

    maps a worker object to DtoUser object

--- src/main/java/com/bumsoap/store/service/worker/WorkerServ.java
+++ src/main/java/com/bumsoap/store/service/worker/WorkerServ.java
@@ -1,16 +1,36 @@
 package com.bumsoap.store.service.worker;
 
+import com.bumsoap.store.dto.EntityConverter;
 import com.bumsoap.store.model.Worker;
 import com.bumsoap.store.repository.WorkerRepoI;
+import com.bumsoap.store.dto.UserDto;
+import com.bumsoap.store.service.photo.PhotoServInt;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class WorkerServ implements WorkerServInt {
     private final WorkerRepoI workerRepo;
+    private final EntityConverter<Worker, UserDto> entityConverter;
+    private final PhotoServInt photoServ;
+
+    private UserDto mapWorkerToDtoUser(Worker worker) {
+        UserDto dtoUser = entityConverter.mapEntityToDto(worker, UserDto.class);
+
+        if (worker.getPhoto() != null) {
+            try {
+                byte[] photoBytes = photoServ.getImageData(worker.getPhoto().getId());
+                dtoUser.setPhotoBytes(photoBytes);
+            } catch (SQLException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+        return dtoUser;
+    }
 
     @Override
     public List<String> findAllDept() {
