06b6865fe0aff88d9b23316103b72bb90f845c8a
박종범 <jbpark03@gmail.com>
2025-04-06 09:45:53 +0900

    define converter to String for localDateTime

--- src/main/java/com/bumsoap/store/util/BsUtils.java
+++ src/main/java/com/bumsoap/store/util/BsUtils.java
@@ -1,7 +1,10 @@
 package com.bumsoap.store.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class BsUtils {
     private static final int EXPIRE_MIN = 10;
@@ -11,5 +14,12 @@ public class BsUtils {
         calendar.setTimeInMillis(new Date().getTime());
         calendar.add(Calendar.MINUTE, EXPIRE_MIN);
         return new Date(calendar.getTime().getTime());
+    }
+
+    public static String getLocalDateTimeStr(LocalDateTime ldTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
+                "yyyy년 MM월 dd일 HH시 mm분 ss초", Locale.KOREAN);
+
+        return ldTime.format(formatter);
     }
 }
