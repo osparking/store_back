fe9753f4afaf4390bb34e1edd320644d2863dad8
박종범 <jbpark03@gmail.com>
2025-04-12 08:15:41 +0900

    checks if login user is qualified to access userId

--- src/main/java/com/bumsoap/store/util/BsUtils.java
+++ src/main/java/com/bumsoap/store/util/BsUtils.java
@@ -1,5 +1,9 @@
 package com.bumsoap.store.util;
 
+import com.bumsoap.store.exception.NOT_QUALIFIED_EX;
+import com.bumsoap.store.security.user.BsUserDetails;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
@@ -21,5 +25,19 @@ public class BsUtils {
                 "yyyy년 MM월 dd일 HH시 mm분 ss초", Locale.KOREAN);
 
         return ldTime.format(formatter);
+    }
+
+    /**
+     * 현재 로그인한 유저가 특정 유저(ID)의 정보레 접근할 자격이 있는지 판단.
+     * @param userId 특정 유저 ID
+     * @return 자격 유무 - 참: 자격이 있음. 거짓: 자격이 없음
+     */
+    public static boolean isQualified(Long userId) {
+        var authen = SecurityContextHolder.getContext().getAuthentication();
+        boolean isAdmin = authen.getAuthorities().stream().anyMatch(
+                role -> "ROLE_ADMIN".equals(role.toString()));
+        Long loginId = ((BsUserDetails)authen.getPrincipal()).getId();
+
+        return (userId == loginId || isAdmin);
     }
 }
