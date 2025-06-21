33dfc6d4ef4bfd115e8eed7b778c709ef1e41759
박종범 <jbpark03@gmail.com>
2025-06-21 11:28:21 +0900

    insert signup_method in select target list

--- src/main/java/com/bumsoap/store/repository/UserRepoI.java
+++ src/main/java/com/bumsoap/store/repository/UserRepoI.java
@@ -23,7 +23,8 @@ public interface UserRepoI extends JpaRepository<BsUser, Long> {
 
     String selectUserDto =
             "select u.id, u.full_name, u.mb_phone, u.email, u.enabled," +
-            " w.dept, u.user_type, u.add_date, e.photo_id, p.image " +
+            " w.dept, u.user_type, u.sign_up_method, u.add_date," +
+            " e.photo_id, p.image " +
             "from bs_user u" +
             " left join employee e on e.employee_id = u.id" +
             " left join worker w on w.worker_id = e.employee_id" +
