fdbe10fc4ad869e8d720e3d7f155dbd5c697be64
박종범 <jbpark03@gmail.com>
2025-07-23 04:24:16 +0900

    convert StoreIngre list into StoreIngreDto list

--- src/main/java/com/bumsoap/store/controller/StoreIngreCon.java
+++ src/main/java/com/bumsoap/store/controller/StoreIngreCon.java
@@ -1,5 +1,8 @@
 package com.bumsoap.store.controller;
 
+import com.bumsoap.store.dto.EntityConverter;
+import com.bumsoap.store.dto.StoreIngreDto;
+import com.bumsoap.store.model.StoreIngre;
 import com.bumsoap.store.repository.StoreIngreRepoI;
 import com.bumsoap.store.response.ApiResp;
 import com.bumsoap.store.util.UrlMap;
@@ -9,6 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RestController
@@ -16,12 +22,18 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 @RequiredArgsConstructor
 public class StoreIngreCon {
   private final StoreIngreRepoI storeIngreRepo;
+  private final EntityConverter<StoreIngre, StoreIngreDto> stInConverter;
 
   @GetMapping(UrlMap.GET_ALL)
   public ResponseEntity<ApiResp> getAllStoredIngred() {
     try {
+      var storedIngre = storeIngreRepo.findAll();
+      List<StoreIngreDto> storeIngreDtos = storedIngre.stream()
+          .map(entity -> stInConverter.mapEntityToDto
+              (entity, StoreIngreDto.class))
+          .collect(Collectors.toList());
       return ResponseEntity.ok().body(
-          new ApiResp("입고 재료 전체 목록", storeIngreRepo.findAll()));
+          new ApiResp("입고 재료 전체 목록", storeIngreDtos));
     } catch (Exception e) {
       return ResponseEntity.status(INTERNAL_SERVER_ERROR)
           .body(new ApiResp(e.getMessage(), null));
