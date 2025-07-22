87bfb0030ceabe454785234d5316848a175f6647
박종범 <jbpark03@gmail.com>
2025-07-22 16:15:43 +0900

    model class for ingred' store history

---
+++ src/main/java/com/bumsoap/store/model/StoreIngre.java
@@ -0,0 +1,41 @@
+package com.bumsoap.store.model;
+
+import com.bumsoap.store.util.PackUnit;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class StoreIngre {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+  private String ingreName; // 재료명; 제공
+  private int quantity; // 용량
+  private PackUnit packunit; // 용량 단위
+  private int count; // 수량
+  private LocalDate storeDate;  // 입고일
+  @CreationTimestamp
+  private LocalDateTime addTime;  // 자료 입력 일시, 자동 부여
+  private String buyPlace; // 구매처, 제공
+  private long workerId; // 정보 입력 직원ID
+  /**
+   * 만료일 - 제조일, 유효 기한, 소비 기한 등에서 도출
+    */
+  private LocalDate expireDate;
+}
+
+
