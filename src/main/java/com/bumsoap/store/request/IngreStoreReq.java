a898322d41b6a8c936fa247ef9370c0a37c3bd3f
박종범 <jbpark03@gmail.com>
2025-07-23 10:11:00 +0900

    ingred' store entry request

---
+++ src/main/java/com/bumsoap/store/request/IngreStoreReq.java
@@ -0,0 +1,17 @@
+package com.bumsoap.store.request;
+
+import com.bumsoap.store.util.PackUnit;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+public class IngreStoreReq {
+  private String ingreName; // 재료명; 제공; 필수
+  private int quantity; // 용량; 필수
+  private PackUnit packunit; // 용량 단위; 필수
+  private int count; // 수량; 필수
+  private LocalDate storeDate;  // 입고일
+  private String buyPlace; // 구매처, 제공; 필수
+  private long workerId; // 정보 입력 직원ID; 필수
+  private LocalDate expireDate;
+}
