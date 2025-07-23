0bdb61b4923e83a967ddbf0ff0db42f375b08831
박종범 <jbpark03@gmail.com>
2025-07-23 18:15:28 +0900

    ingred store service of delete record

---
+++ src/main/java/com/bumsoap/store/service/store/StoreIngreServ.java
@@ -0,0 +1,27 @@
+package com.bumsoap.store.service.store;
+
+import com.bumsoap.store.exception.IdNotFoundEx;
+import com.bumsoap.store.repository.StoreIngreRepoI;
+import com.bumsoap.store.util.Feedback;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StoreIngreServ implements StoreIngreServI {
+  private final StoreIngreRepoI storeIngreRepo;
+
+  @Override
+  public String deleteById(Long id) {
+    var storedIngre = storeIngreRepo.findById(id);
+    if (storedIngre.isPresent()) {
+      var storedIngreIns = storedIngre.get();
+
+      storeIngreRepo.delete(storedIngreIns);
+      return storedIngreIns.getIngreName();
+    } else {
+      throw new IdNotFoundEx(Feedback.ST_RE_ID_NOT_FOUND + id);
+    }
+  }
+
+}
---
+++ src/main/java/com/bumsoap/store/service/store/StoreIngreServI.java
@@ -0,0 +1,5 @@
+package com.bumsoap.store.service.store;
+
+public interface StoreIngreServI {
+  String deleteById(Long id);
+}
