235f051a4934151b8354291c3058769b128ff7f0
박종범 <jbpark03@gmail.com>
2025-07-24 15:47:21 +0900

    finds ingred store record by the ID

--- src/main/java/com/bumsoap/store/service/store/StoreIngreServ.java
+++ src/main/java/com/bumsoap/store/service/store/StoreIngreServ.java
@@ -1,6 +1,7 @@
 package com.bumsoap.store.service.store;
 
 import com.bumsoap.store.exception.IdNotFoundEx;
+import com.bumsoap.store.model.StoreIngre;
 import com.bumsoap.store.repository.StoreIngreRepoI;
 import com.bumsoap.store.util.Feedback;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class StoreIngreServ implements StoreIngreServI {
   private final StoreIngreRepoI storeIngreRepo;
+
+  @Override
+  public StoreIngre findById(Long id) {
+    return storeIngreRepo.findById(id).orElseThrow(() ->
+        new IdNotFoundEx(Feedback.ST_RE_ID_NOT_FOUND + id));
+  }
 
   @Override
   public String deleteById(Long id) {
