f68f9b0a3045e424523fcbdec3f5cf02e3883165
박종범 <jbpark03@gmail.com>
2025-07-24 18:04:33 +0900

    get distinct ingredient names

--- src/main/java/com/bumsoap/store/repository/StoreIngreRepoI.java
+++ src/main/java/com/bumsoap/store/repository/StoreIngreRepoI.java
@@ -2,8 +2,14 @@ package com.bumsoap.store.repository;
 
 import com.bumsoap.store.model.StoreIngre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StoreIngreRepoI extends JpaRepository<StoreIngre, Long> {
+  @Query(nativeQuery = true,
+      value = "select distinct si.ingre_name from store_ingre si ")
+  List<String> findDistinctIngreNames();
 }
