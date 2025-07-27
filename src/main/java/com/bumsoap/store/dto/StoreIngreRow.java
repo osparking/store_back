408c961c2489f4e2d31bdd12400869ec355d6396
박종범 <jbpark03@gmail.com>
2025-07-27 16:04:39 +0900

    error fix - result conversion, timestamp conversion

--- src/main/java/com/bumsoap/store/dto/StoreIngreRow.java
+++ src/main/java/com/bumsoap/store/dto/StoreIngreRow.java
@@ -1,11 +1,14 @@
 package com.bumsoap.store.dto;
 
 import com.bumsoap.store.model.StoreIngre;
-import jakarta.persistence.Entity;
+import com.bumsoap.store.util.PackUnit;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.sql.Date;
+import java.sql.Timestamp;
 
 @Getter
 @Setter
@@ -13,4 +16,16 @@ import lombok.Setter;
 @NoArgsConstructor
 public class StoreIngreRow extends StoreIngre {
   private String workerName;
+
+  public StoreIngreRow(Long id, String ingreName, int quantity,
+                       byte packunit, int count,
+                       Date storeDate, Timestamp addTime,
+                       String buyPlace, long workerId,
+                       Date expireDate, String workerName) {
+    super(id, ingreName, quantity, PackUnit.values()[packunit],
+        count, storeDate.toLocalDate(),
+        addTime.toLocalDateTime(), buyPlace,
+        workerId, expireDate.toLocalDate());
+    this.workerName = workerName;
+  }
 }
