b35064beebb15e71d5d05a63492cf45451607723
박종범 <jbpark03@gmail.com>
2025-08-02 21:11:12 +0900

    bumsoap stock inventory entity class

---
+++ src/main/java/com/bumsoap/store/model/SoapInven.java
@@ -0,0 +1,29 @@
+package com.bumsoap.store.model;
+
+import com.bumsoap.store.util.BsShape;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SoapInven {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(nullable = false)
+  private BsShape bsShape;
+  @Column(nullable = false)
+  private int stockLevel;
+  @Column(nullable = false)
+  private BigDecimal unitPrice;
+  @Column(name = "add_time", updatable = false, nullable = false,
+      columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+  private LocalDateTime addTime = LocalDateTime.now();
+}
