c501cb5cc43374705484afe0e22563770289d26b
박종범 <jbpark03@gmail.com>
2025-08-03 11:02:40 +0900

    declare bsShape field is unique, table has only 3 records

--- src/main/java/com/bumsoap/store/model/SoapInven.java
+++ src/main/java/com/bumsoap/store/model/SoapInven.java
@@ -14,7 +14,7 @@ public class SoapInven {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-  @Column(nullable = false)
+  @Column(nullable = false, unique = true)
   private BsShape bsShape;
   @Column(nullable = false)
   private int stockLevel;
