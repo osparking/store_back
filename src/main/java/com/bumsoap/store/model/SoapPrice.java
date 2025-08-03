e90eac9dc975b438e44b9377c17a96ed56814598
박종범 <jbpark03@gmail.com>
2025-08-03 10:38:22 +0900

    soap price history per soap shape

---
+++ src/main/java/com/bumsoap/store/model/SoapPrice.java
@@ -0,0 +1,32 @@
+package com.bumsoap.store.model;
+
+import com.bumsoap.store.util.BsShape;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SoapPrice {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(nullable = false)
+  private BsShape bsShape;
+  @Column(nullable = false)
+  private BigDecimal unitPrice;
+  @Column(name = "add_time", updatable = false, nullable = false,
+      columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+  private LocalDateTime applyTime = LocalDateTime.now();
+
+  public SoapPrice(BsShape bsShape, BigDecimal unitPrice) {
+    this.bsShape = bsShape;
+    this.unitPrice = unitPrice;
+  }
+}
