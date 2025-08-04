d756d2eda3b3f00eddfbc663669fb857f62a87c3
박종범 <jbpark03@gmail.com>
2025-08-04 10:34:34 +0900

    data to request current soap price

--- src/main/java/com/bumsoap/store/request/SoapPriceReq.java
+++ src/main/java/com/bumsoap/store/request/SoapPriceReq.java
@@ -2,17 +2,14 @@ package com.bumsoap.store.request;
 
 import com.bumsoap.store.util.BsShape;
 import lombok.Data;
+
 import java.math.BigDecimal;
 
 @Data
-public class SoapPriceAddReq {
+public class SoapPriceReq {
   private BsShape bsShape;
-  private BigDecimal unitPrice;
 
   public void setBsShape(String bsShapelabel) {
     this.bsShape = BsShape.valueOfLabel(bsShapelabel);
-  }
-  public void setUnitPrice(String priceStr) {
-    this.unitPrice = new BigDecimal(priceStr);
   }
 }
