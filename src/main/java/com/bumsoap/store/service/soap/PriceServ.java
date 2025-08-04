d863ea71a41106ba92ed65a14efaa95416ee4e00
박종범 <jbpark03@gmail.com>
2025-08-04 10:26:27 +0900

    return price service method and inter'

--- src/main/java/com/bumsoap/store/service/soap/PriceServ.java
+++ src/main/java/com/bumsoap/store/service/soap/PriceServ.java
@@ -2,13 +2,21 @@ package com.bumsoap.store.service.soap;
 
 import com.bumsoap.store.model.SoapPrice;
 import com.bumsoap.store.repository.SoapPriceRepo;
+import com.bumsoap.store.util.BsShape;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
 public class PriceServ implements PriceServI{
   private final SoapPriceRepo soapRepo;
+
+  @Override
+  public BigDecimal findSoapPrice(BsShape bsShape) {
+    return soapRepo.findSoapPrice(bsShape.ordinal());
+  }
 
   @Override
   public SoapPrice add(SoapPrice soapInven) {
--- src/main/java/com/bumsoap/store/service/soap/PriceServI.java
+++ src/main/java/com/bumsoap/store/service/soap/PriceServI.java
@@ -1,7 +1,12 @@
 package com.bumsoap.store.service.soap;
 
 import com.bumsoap.store.model.SoapPrice;
+import com.bumsoap.store.util.BsShape;
+
+import java.math.BigDecimal;
 
 public interface PriceServI {
+  BigDecimal findSoapPrice(BsShape bsShape);
+
   SoapPrice add(SoapPrice soapInven);
 }
