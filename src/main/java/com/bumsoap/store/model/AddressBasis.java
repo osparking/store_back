3ce34e4a2d779d831b27b0d55b7e0c044870e6c0
BumTiger <jbpark03@gmail.com>
2025-08-05 17:18:42 +0900

    declare address basis class an entity

--- src/main/java/com/bumsoap/store/model/AddressBasis.java
+++ src/main/java/com/bumsoap/store/model/AddressBasis.java
@@ -1,12 +1,21 @@
 package com.bumsoap.store.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@Entity
+@NoArgsConstructor
 public class AddressBasis {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
   private String zipcode;
+  @Column(nullable = false, unique = true)
   private String roadAddress; // 도로명 주소
+  @Column(nullable = false)
   private String zBunAddress; // 지번 주소
 }
