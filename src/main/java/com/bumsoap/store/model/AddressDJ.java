55c982f58b48b3e6c2ab3744c4eca19652da0ac1
BumTiger <jbpark03@gmail.com>
2025-08-05 11:31:46 +0900

    address item class - 3 members/fields

---
+++ src/main/java/com/bumsoap/store/model/AddressDJ.java
@@ -0,0 +1,12 @@
+package com.bumsoap.store.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AddressDJ {
+  private String zipcode;
+  private String roadAddress; // 도로명 주소
+  private String zBunAddress; // 지번 주소
+}
