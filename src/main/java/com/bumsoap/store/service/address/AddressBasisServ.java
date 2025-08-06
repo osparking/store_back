7e33ad79b78e635cb1dddf896cefac984caf494f
BumTiger <jbpark03@gmail.com>
2025-08-06 14:44:32 +0900

    return address basis residing in DB

--- src/main/java/com/bumsoap/store/service/address/AddressBasisServ.java
+++ src/main/java/com/bumsoap/store/service/address/AddressBasisServ.java
@@ -19,4 +19,21 @@ public class AddressBasisServ implements AddressBasisServI{
   public AddressBasis findByRoadAddress(String roadAddress) {
     return addrBasisRepo.findByRoadAddress(roadAddress).orElse(null);
   }
+
+  /**
+   * 추가하려는 기초 주소가 이미 DB 에 있으면, 그 것을,
+   * 아니면(없으면), 삽입한 뒤 그 반환값을 돌려준다.
+   *
+   * @param addressBasis 삽입하려는 기초 주소
+   * @return (결과적으로) DB에 존재하는 기초 주소
+   */
+  @Override
+  public AddressBasis addAddressBasis(AddressBasis addressBasis) {
+    // 테이블에 같은 주소 존재 가능성 타진
+    var addressInDB = addrBasisRepo.findByRoadAddress(
+        addressBasis.getRoadAddress());
+    return addressInDB.isPresent()
+        ? addressInDB.get()
+        : addrBasisRepo.save(addressBasis);
+  }
 }
