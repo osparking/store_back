62ab36989be11d70df12077c0202a4404a9a1124
BumTiger <jbpark03@gmail.com>
2025-08-06 10:26:40 +0900

    entity class of order recipient info

---
+++ src/main/java/com/bumsoap/store/model/Recipient.java
@@ -0,0 +1,42 @@
+package com.bumsoap.store.model;
+
+import com.bumsoap.store.util.DoroZbun;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Recipient {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(nullable = false)
+  private String fullName;
+  private String mbPhone; // Mobile Phone
+
+  @ManyToOne
+  @JoinColumn(name="addr_basis_id", nullable = false)
+  private AddressBasis addressBasis; // address basisID
+
+  @Column(nullable = false)
+  private DoroZbun doroZbun = DoroZbun.ROAD;
+  private String addrDetail; // Detailed address
+
+  public Recipient(String fullName,
+                   String mbPhone,
+                   AddressBasis addressBasis,
+                   DoroZbun doroZbun,
+                   String addrDetail) {
+    this.fullName = fullName;
+    this.mbPhone = mbPhone;
+    this.addressBasis = addressBasis;
+    this.doroZbun = doroZbun;
+    this.addrDetail = addrDetail;
+  }
+}
