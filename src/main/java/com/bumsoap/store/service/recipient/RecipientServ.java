6fa79a328f0052a6dd23b709c21c58b52134823f
BumTiger <jbpark03@gmail.com>
2025-08-06 16:09:06 +0900

    serve w/ recipient info save function

---
+++ src/main/java/com/bumsoap/store/service/recipient/RecipientServ.java
@@ -0,0 +1,17 @@
+package com.bumsoap.store.service.recipient;
+
+import com.bumsoap.store.model.Recipient;
+import com.bumsoap.store.repository.RecipientRepoI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RecipientServ implements RecipientServI {
+  private final RecipientRepoI recipientRepo;
+
+  @Override
+  public Recipient save(Recipient recipient) {
+    return recipientRepo.save(recipient);
+  }
+}
---
+++ src/main/java/com/bumsoap/store/service/recipient/RecipientServI.java
@@ -0,0 +1,7 @@
+package com.bumsoap.store.service.recipient;
+
+import com.bumsoap.store.model.Recipient;
+
+public interface RecipientServI {
+  Recipient save(Recipient recipient);
+}
