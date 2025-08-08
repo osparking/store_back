2bd5fd58baa191a25c9362fbb0398d0f4a06bd7e
BumTiger <jbpark03@gmail.com>
2025-08-08 11:56:20 +0900

    ten order status are defined

---
+++ src/main/java/com/bumsoap/store/util/OrderStatus.java
@@ -0,0 +1,34 @@
+package com.bumsoap.store.util;
+
+public enum OrderStatus {
+  SHOP_CART("장바구니"),
+  ORDER_PAID("주문"),
+  SELLER_RECOGNIZED("판매자 인지"),
+  AT_GS25("GS25 접수"),
+  COLLECTED("배송기사수거"),
+  IN_DELIVERY("배송 중"),
+  DELIVERED("배송 완료"),
+  PURCHASE_CONFIRMED("구매 확정"),
+  CANCELLED("취소됨"),
+  REFUNDED("환불됨");
+
+  public final String label;
+
+  public static OrderStatus valueOfLabel(String label) {
+    for (OrderStatus e : values()) {
+      if (e.label.equals(label)) {
+        return e;
+      }
+    }
+    return null;
+  }
+
+  private OrderStatus(String label) {
+    this.label = label;
+  }
+
+  @Override
+  public String toString() {
+    return label;
+  }
+}
