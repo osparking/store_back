afa3f9bea94384012710625ae18c57b3f3911af3
BumTiger <jbpark03@gmail.com>
2025-08-09 17:19:15 +0900

    order saving service method completed

--- src/main/java/com/bumsoap/store/service/order/OrderServ.java
+++ src/main/java/com/bumsoap/store/service/order/OrderServ.java
@@ -1,19 +1,30 @@
 package com.bumsoap.store.service.order;
 
+import com.bumsoap.store.exception.InventoryException;
 import com.bumsoap.store.model.BsOrder;
+import com.bumsoap.store.model.OrderItem;
 import com.bumsoap.store.repository.OrderRepo;
+import com.bumsoap.store.service.orderItem.OrderItemServI;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OrderServ implements OrderServI {
   private final OrderRepo orderRepo;
+  private final OrderItemServI orderItemServ;
 
   @Override
+  @Transactional(rollbackOn = InventoryException.class)
   public BsOrder saveOrder(BsOrder order) {
     BsOrder savedOrder = orderRepo.save(order);
+    order.getItems().forEach(item -> item.setOrder(savedOrder));
+
+    List<OrderItem> savedItems = orderItemServ.saveOrderItems(order.getItems());
+    order.setItems(savedItems);
     return savedOrder;
   }
 }
-
