91163dd231a2279af4d002c58f5fccb4a38718ca
BumTiger <jbpark03@gmail.com>
2025-08-09 15:30:46 +0900

    add order request info class

---
+++ src/main/java/com/bumsoap/store/request/AddOrderReq.java
@@ -0,0 +1,22 @@
+package com.bumsoap.store.request;
+
+import com.bumsoap.store.model.Recipient;
+import com.bumsoap.store.util.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class AddOrderReq {
+  private Long id;
+  private Long userId;
+  private List<AddOrderItemReq> items; // 예, 백설공주 2개
+  private Recipient recipient;
+  private String orderStatus;
+
+  public OrderStatus getOrderStatus() {
+    return OrderStatus.valueOfLabel(orderStatus);
+  }
+}
