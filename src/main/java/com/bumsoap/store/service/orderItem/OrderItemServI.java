2f15360580133b27fe841c9a11f35501335d1c73
BumTiger <jbpark03@gmail.com>
2025-08-09 19:30:40 +0900

    copy method heading to interface

--- src/main/java/com/bumsoap/store/service/orderItem/OrderItemServI.java
+++ src/main/java/com/bumsoap/store/service/orderItem/OrderItemServI.java
@@ -2,6 +2,10 @@ package com.bumsoap.store.service.orderItem;
 
 import com.bumsoap.store.model.OrderItem;
 
+import java.util.List;
+
 public interface OrderItemServI {
   OrderItem save(OrderItem orderItem);
+
+  List<OrderItem> saveOrderItems(List<OrderItem> items);
 }
