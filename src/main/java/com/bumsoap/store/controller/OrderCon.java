06c817984d378aa18fc4a4aaac269971edc50c14
BumTiger <jbpark03@gmail.com>
2025-08-09 15:50:08 +0900

    use order saving service method skeleton

--- src/main/java/com/bumsoap/store/controller/OrderCon.java
+++ src/main/java/com/bumsoap/store/controller/OrderCon.java
@@ -1,13 +1,16 @@
 package com.bumsoap.store.controller;
 
 import com.bumsoap.store.dto.ObjMapper;
+import com.bumsoap.store.exception.IdNotFoundEx;
 import com.bumsoap.store.exception.InventoryException;
 import com.bumsoap.store.model.AddressBasis;
 import com.bumsoap.store.model.BsOrder;
+import com.bumsoap.store.repository.UserRepoI;
 import com.bumsoap.store.request.AddOrderReq;
 import com.bumsoap.store.request.AddrBasisAddReq;
 import com.bumsoap.store.response.ApiResp;
 import com.bumsoap.store.service.address.AddressBasisServI;
+import com.bumsoap.store.service.order.OrderServI;
 import com.bumsoap.store.util.Feedback;
 import com.bumsoap.store.util.UrlMap;
 import lombok.RequiredArgsConstructor;
@@ -28,13 +31,18 @@ public class OrderCon {
   @Autowired
   private final AddressBasisServI addrBasisServ;
   private final ObjMapper objMapper;
+  private final OrderServI orderServ;
+  private final UserRepoI userRepo;
 
   @PostMapping(UrlMap.ADD)
   public ResponseEntity<ApiResp> addOrder(
       @RequestBody AddOrderReq addOrderReq) {
     try {
       BsOrder order = objMapper.mapToDto(addOrderReq, BsOrder.class);
-      BsOrder orderSaved = null;
+      var user = userRepo.findById(addOrderReq.getUserId()).orElseThrow(
+          () -> new IdNotFoundEx(Feedback.USER_ID_NOT_FOUND));
+      order.setUser(user);
+      BsOrder orderSaved = orderServ.saveOrder(order);
 
       return ResponseEntity.ok(new ApiResp(Feedback.SOAP_ORDER_SAVED,
           orderSaved));
