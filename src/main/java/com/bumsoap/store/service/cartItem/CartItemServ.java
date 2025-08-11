f57f2c64cab03f64bd985b25d3c864e603426de0
BumTiger <jbpark03@gmail.com>
2025-08-11 09:37:36 +0900

    use subtotaler.getSubtotal() method

--- src/main/java/com/bumsoap/store/service/cartItem/CartItemServ.java
+++ src/main/java/com/bumsoap/store/service/cartItem/CartItemServ.java
@@ -6,13 +6,11 @@ import com.bumsoap.store.exception.InventoryException;
 import com.bumsoap.store.model.CartItem;
 import com.bumsoap.store.repository.CartItemRepo;
 import com.bumsoap.store.service.soap.InvenServI;
-import com.bumsoap.store.service.soap.PriceServI;
 import com.bumsoap.store.util.Feedback;
+import com.bumsoap.store.util.SubTotaler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.List;
 
 @Service
@@ -21,7 +19,7 @@ public class CartItemServ implements CartItemServI {
   private final CartItemRepo cartItemRepo;
   private final InvenServI invenServ;
   private final ObjMapper objMapper;
-  private final PriceServI priceServ;
+  private final SubTotaler subTotaler;
 
   @Override
   public CartItem saveItem(CartItem item) {
@@ -45,26 +43,9 @@ public class CartItemServ implements CartItemServI {
     return cartItemRepo.findByUserId(uid).stream()
         .map(item -> {
           CartItemDto dto = objMapper.mapToDto(item, CartItemDto.class);
-          dto.setSubTotal(getSubtotal(dto));
+          dto.setSubTotal(subTotaler.getSubtotal(dto));
           return dto;
         })
         .toList();
-  }
-
-  private BigDecimal getSubtotal(CartItemDto item) {
-    /**
-     * 재고 검사 및 예외 발생
-     */
-    int level = invenServ.getByBsShape(item.getShape()).getStockLevel();
-    if (level < item.getCount()) {
-      StringBuffer sb = new StringBuffer(Feedback.SHORT_INVENTORY);
-      sb.append(item.getShape().label);
-      sb.append(" - ");
-      sb.append(level);
-      throw new InventoryException(sb.toString());
-    }
-    var subTotal = priceServ.findSoapPrice(item.getShape())
-        .multiply(BigDecimal.valueOf(item.getCount()));
-    return subTotal.setScale(0, RoundingMode.HALF_UP);
   }
 }
