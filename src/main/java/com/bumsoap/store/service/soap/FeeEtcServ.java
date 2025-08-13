4124f751493058e7bdd76ed2cfa7617add47ed60
BumTiger <jbpark03@gmail.com>
2025-08-13 17:10:13 +0900

    fee etc row adding service class and inter'

---
+++ src/main/java/com/bumsoap/store/service/soap/FeeEtcServ.java
@@ -0,0 +1,17 @@
+package com.bumsoap.store.service.soap;
+
+import com.bumsoap.store.model.FeeEtc;
+import com.bumsoap.store.repository.FeeEtcRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FeeEtcServ implements FeeEtcServI{
+  private final FeeEtcRepo feeEtcRepo;
+
+  @Override
+  public FeeEtc add(FeeEtc feeEtc) {
+    return feeEtcRepo.save(feeEtc);
+  }
+}
---
+++ src/main/java/com/bumsoap/store/service/soap/FeeEtcServI.java
@@ -0,0 +1,7 @@
+package com.bumsoap.store.service.soap;
+
+import com.bumsoap.store.model.FeeEtc;
+
+public interface FeeEtcServI {
+  FeeEtc add(FeeEtc soapInven);
+}
