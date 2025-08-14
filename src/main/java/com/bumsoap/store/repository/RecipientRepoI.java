24632a6b0277c0415005f1ec22fb3573f124de5d
BumTiger <jbpark03@gmail.com>
2025-08-14 22:15:34 +0900

    return id of identical recipient record

--- src/main/java/com/bumsoap/store/repository/RecipientRepoI.java
+++ src/main/java/com/bumsoap/store/repository/RecipientRepoI.java
@@ -2,6 +2,20 @@ package com.bumsoap.store.repository;
 
 import com.bumsoap.store.model.Recipient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RecipientRepoI extends JpaRepository<Recipient, Long> {
+  @Query(nativeQuery = true, value =
+      "select r.id from recipient r where " +
+          "r.addr_basis_id = :addr_basis_id " +
+          "and r.address_detail = :address_detail " +
+          "and r.doro_zbun = :doro_zbun " +
+          "and r.full_name = :full_name " +
+          "and r.mb_phone = :mb_phone")
+  long getRecipientIdIfExist(@Param("addr_basis_id") Long addr_basis_id,
+                             @Param("address_detail") Long address_detail,
+                             @Param("doro_zbun") Long doro_zbun,
+                             @Param("full_name") Long full_name,
+                             @Param("mb_phone") Long mb_phone);
 }
