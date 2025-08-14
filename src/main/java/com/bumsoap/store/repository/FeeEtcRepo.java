92e2af4d46d08018abf46032e6e6a77fbf92cbb3
BumTiger <jbpark03@gmail.com>
2025-08-14 10:23:19 +0900

    read latest feeEtc table record

--- src/main/java/com/bumsoap/store/repository/FeeEtcRepo.java
+++ src/main/java/com/bumsoap/store/repository/FeeEtcRepo.java
@@ -2,6 +2,10 @@ package com.bumsoap.store.repository;
 
 import com.bumsoap.store.model.FeeEtc;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FeeEtcRepo extends JpaRepository<FeeEtc, Long> {
+  @Query(nativeQuery = true, value =
+      "select fe.* from fee_etc fe order by fe.id desc limit 1")
+  FeeEtc findLatestFee();
 }
