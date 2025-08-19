27dd661e811f7da5eb13e567b947f35cddee86b4
BumTiger <jbpark03@gmail.com>
2025-08-19 14:35:25 +0900

    serves soap shape selection options list

--- src/main/java/com/bumsoap/store/service/soap/InvenServ.java
+++ src/main/java/com/bumsoap/store/service/soap/InvenServ.java
@@ -1,5 +1,7 @@
 package com.bumsoap.store.service.soap;
 
+import com.bumsoap.store.dto.ShapeSelDto;
+import com.bumsoap.store.dto.ShapeSelItem;
 import com.bumsoap.store.exception.DataNotFoundException;
 import com.bumsoap.store.model.SoapInven;
 import com.bumsoap.store.repository.SoapInvenI;
@@ -7,6 +9,9 @@ import com.bumsoap.store.util.BsShape;
 import com.bumsoap.store.util.Feedback;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +21,34 @@ public class InvenServ implements InvenServI {
   @Override
   public SoapInven add(SoapInven soapInven) {
     return soapInvenI.save(soapInven);
+  }
+
+  @Override
+  public ShapeSelDto getShapeSelItems() {
+    List<SoapInven> soapInventories = soapInvenI.findAll();
+    List<ShapeSelItem> inStockList = new ArrayList<>();
+    List<SoapInven> ooStockList = new ArrayList<>();
+
+    for (SoapInven inven : soapInventories) {
+      if (inven.getStockLevel() > 0) {
+        inStockList.add(new ShapeSelItem(
+            inven.getBsShape().label, inven.getStockLevel()));
+      } else {
+        ooStockList.add(inven);
+      }
+    }
+
+    for (SoapInven inven : ooStockList) {
+      inStockList.add(new ShapeSelItem(
+          inven.getBsShape().label + "(품절)", 0));
+    }
+
+    // 외형 선택 항 중, 재고 있는 첫째 항
+    var firstInStock = inStockList.stream()
+        .filter(item -> item.getCount() > 0).findFirst();
+
+    return new ShapeSelDto(inStockList,
+        firstInStock.isPresent() ? firstInStock.get().getShapeLabel() : "");
   }
 
   @Override
--- src/main/java/com/bumsoap/store/service/soap/InvenServI.java
+++ src/main/java/com/bumsoap/store/service/soap/InvenServI.java
@@ -1,10 +1,13 @@
 package com.bumsoap.store.service.soap;
 
+import com.bumsoap.store.dto.ShapeSelDto;
 import com.bumsoap.store.model.SoapInven;
 import com.bumsoap.store.util.BsShape;
 
 public interface InvenServI {
   SoapInven add(SoapInven soapInven);
+
+  ShapeSelDto getShapeSelItems();
 
   SoapInven getByBsShape(BsShape bsShape);
 }
