24ed924c5142036986928f1fce6417b4d3f662d9
BumTiger <jbpark03@gmail.com>
2025-08-20 12:00:32 +0900

    reads soap shape ordinal and price pair list

--- src/main/java/com/bumsoap/store/repository/SoapPriceRepo.java
+++ src/main/java/com/bumsoap/store/repository/SoapPriceRepo.java
@@ -1,5 +1,6 @@
 package com.bumsoap.store.repository;
 
+import com.bumsoap.store.dto.SoapPriceDto;
 import com.bumsoap.store.model.SoapPrice;
 import com.bumsoap.store.util.BsShape;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public interface SoapPriceRepo extends JpaRepository<SoapPrice, Long> {
   int countByBsShape(BsShape bsShape);
@@ -16,4 +18,19 @@ public interface SoapPriceRepo extends JpaRepository<SoapPrice, Long> {
           "where sp.bs_shape = :bsShape " +
           "order by sp.apply_time desc limit 1")
   BigDecimal findSoapPrice(@Param("bsShape") int bsShape);
+
+  @Query(nativeQuery = true,
+      value = (
+          "(select sp.bs_shape, sp.unit_price from soap_price sp" +
+          " where sp.bs_shape = 0" +
+          " order by sp.apply_time desc limit 1) " +
+          "union " +
+          "(select sp.bs_shape, sp.unit_price from soap_price sp" +
+          " where sp.bs_shape = 1" +
+          " order by sp.apply_time desc limit 1) " +
+          "union " +
+          "(select sp.bs_shape, sp.unit_price from soap_price sp" +
+          " where sp.bs_shape = 2" +
+          " order by sp.apply_time desc limit 1)"))
+  List<SoapPriceDto> findSoapPrices();
 }
