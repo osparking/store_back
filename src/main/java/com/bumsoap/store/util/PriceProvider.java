19b66a024c1d629b56c857516f23ac5d99e37663
BumTiger <jbpark03@gmail.com>
2025-08-20 12:04:42 +0900

    provides price of each shape of soap

---
+++ src/main/java/com/bumsoap/store/util/PriceProvider.java
@@ -0,0 +1,28 @@
+package com.bumsoap.store.util;
+
+import com.bumsoap.store.dto.SoapPriceDto;
+import com.bumsoap.store.repository.SoapPriceRepo;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component
+public class PriceProvider {
+  private final Map<Integer, BigDecimal> priceMap;
+
+  public PriceProvider(SoapPriceRepo soapPriceRepo) {
+    List<SoapPriceDto> soapPrices = soapPriceRepo.findSoapPrices();
+    priceMap = soapPrices.stream()
+        .collect(Collectors.toMap(
+            SoapPriceDto::getShapeOrdinal,
+            SoapPriceDto::getUnitPrice
+        ));
+  }
+
+  public BigDecimal getShapePrice(int ordinal) {
+    return priceMap.get(ordinal);
+  }
+}
