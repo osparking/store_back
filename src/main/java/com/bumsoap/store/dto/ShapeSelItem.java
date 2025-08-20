bf3c4cd2984b02bf4f3bc6cb480db167826e9072
BumTiger <jbpark03@gmail.com>
2025-08-20 12:07:43 +0900

    add price for soap selection option item

--- src/main/java/com/bumsoap/store/dto/ShapeSelItem.java
+++ src/main/java/com/bumsoap/store/dto/ShapeSelItem.java
@@ -3,9 +3,12 @@ package com.bumsoap.store.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 @AllArgsConstructor
 public class ShapeSelItem {
   private String shapeLabel;
   private int count;
+  private BigDecimal price;
 }
