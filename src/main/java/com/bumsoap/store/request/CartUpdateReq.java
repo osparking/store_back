5954809af78c071c049b140af5fa1a1be13d97ac
BumTiger <jbpark03@gmail.com>
2025-08-27 15:52:56 +0900

    one user cart update request class

---
+++ src/main/java/com/bumsoap/store/request/CartUpdateReq.java
@@ -0,0 +1,23 @@
+package com.bumsoap.store.request;
+
+import com.bumsoap.store.dto.CartItemCount;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+/* 요청 Json 사례:
+   { "deleteId": [53],
+     "updateCount": [
+        {"id": 54, "count": 5 },
+        {"id": 56, "count": 2 }]
+   }
+*/
+public class CartUpdateReq extends AddItemReq {
+  private Long[] deleteId;
+  private CartItemCount[] updateCount;
+}
