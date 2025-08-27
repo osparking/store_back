e0052087681b8737a94f4005b6c0c51764f5e4f8
BumTiger <jbpark03@gmail.com>
2025-08-27 15:33:06 +0900

    change cart update request example

--- src/main/java/com/bumsoap/store/request/CartUpdateReq.java
+++ src/main/java/com/bumsoap/store/request/CartUpdateReq.java
@@ -10,23 +10,13 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-/*
-  {
-      "deleteId": [
-          53
-      ],
-      "updateCount": [
-          {
-              "id": 54,
-              "count": 5
-          },
-          {
-              "id": 56,
-              "count": 2
-          }
-      ]
-  }
- */
+/* 요청 Json 사례:
+   { "deleteId": [53],
+     "updateCount": [
+        {"id": 54, "count": 5 },
+        {"id": 56, "count": 2 }]
+   }
+*/
 public class CartUpdateReq extends AddItemReq {
   private Long[] deleteId;
   private CartItemCount[] updateCount;
