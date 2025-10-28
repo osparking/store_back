3b14c8a22fc8c5eb6b2c14708ca4e5a827e4b17e
BumTiger <jbpark03@gmail.com>
2025-10-28 15:39:09 +0900

    explains data delivery fee calc request carries

--- src/main/java/com/bumsoap/store/request/DeliveryFeeReq.java
+++ src/main/java/com/bumsoap/store/request/DeliveryFeeReq.java
@@ -9,6 +9,11 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+/*
+ 배송비 계산 요청 바디 자료
+ 우편번호와 총 금액에 의하여 배송비는 결정된다.
+ 예) {"zipcode":"12915","grandTotal":3900 }
+ */
 public class DeliveryFeeReq {
   private String zipcode;
   private BigDecimal grandTotal;
