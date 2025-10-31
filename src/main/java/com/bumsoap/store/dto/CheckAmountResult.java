c4595e7f411a648172e90b6461cb7dcbf3839d3e
BumTiger <jbpark03@gmail.com>
2025-10-31 20:57:05 +0900

    says if payment amount matches session saved one

---
+++ src/main/java/com/bumsoap/store/dto/CheckAmountResult.java
@@ -0,0 +1,12 @@
+package com.bumsoap.store.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CheckAmountResult {
+    private boolean matches;
+}
