755d3602ef5e7c8239dfcb143b83adf013af840a
BumTiger <jbpark03@gmail.com>
2025-11-01 07:01:33 +0900

    increase security by using requestBody

--- src/main/java/com/bumsoap/store/controller/PaymentCon.java
+++ src/main/java/com/bumsoap/store/controller/PaymentCon.java
@@ -1,14 +1,12 @@
 package com.bumsoap.store.controller;
 
 import com.bumsoap.store.dto.CheckAmountResult;
+import com.bumsoap.store.request.SaveAmountReq;
 import com.bumsoap.store.util.UrlMap;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
@@ -19,15 +17,15 @@ public class PaymentCon {
     @PostMapping("/checkAmount")
     public ResponseEntity<CheckAmountResult> checkIfAmountMatches(
             HttpSession session,
-            @RequestParam String orderId,
-            @RequestParam BigDecimal amount
-    ) {
-        var savedAmount = (BigDecimal) session.getAttribute(orderId);
+            @RequestBody SaveAmountReq orderAmount) {
+        var savedAmount = (BigDecimal) session
+                .getAttribute(orderAmount.getOrderId());
         if (savedAmount==null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
                     new CheckAmountResult(false));
         } else
             return ResponseEntity.status(HttpStatus.OK).body(
-                    new CheckAmountResult(amount.equals(savedAmount)));
+                    new CheckAmountResult(orderAmount.getAmount()
+                            .equals(savedAmount)));
     }
 }
