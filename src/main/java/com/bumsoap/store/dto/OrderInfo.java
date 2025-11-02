4e3bfb6f19adc520fce2e8d2fac0fc8493cf56f2
BumTiger <jbpark03@gmail.com>
2025-11-02 19:03:08 +0900

    class for session saved order info'

---
+++ src/main/java/com/bumsoap/store/dto/OrderInfo.java
@@ -0,0 +1,15 @@
+package com.bumsoap.store.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderInfo {
+    private BigDecimal amount;
+    private String orderName;
+}
