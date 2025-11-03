827af81259ac487f0ed80f0e3ffffc2a77c82af3
BumTiger <jbpark03@gmail.com>
2025-11-03 13:43:50 +0900

    when orderId given, serves order info for that

--- src/main/java/com/bumsoap/store/service/order/OrderServ.java
+++ src/main/java/com/bumsoap/store/service/order/OrderServ.java
@@ -2,6 +2,7 @@ package com.bumsoap.store.service.order;
 
 import com.bumsoap.store.exception.IdNotFoundEx;
 import com.bumsoap.store.exception.InventoryException;
+import com.bumsoap.store.exception.OrderIdNotFoundEx;
 import com.bumsoap.store.model.BsOrder;
 import com.bumsoap.store.model.FeeEtc;
 import com.bumsoap.store.model.OrderItem;
@@ -41,6 +42,17 @@ public class OrderServ implements OrderServI {
 
   @PersistenceContext
   private EntityManager entityManager;
+
+  public BsOrder getOrderByOrderId(String orderId)
+          throws OrderIdNotFoundEx {
+    var optionalOrder = orderRepo.findByOrderId(orderId);
+
+    if (optionalOrder.isPresent()) {
+      return optionalOrder.get();
+    } else {
+      throw new OrderIdNotFoundEx(Feedback.ORDER_ID_NOT_FOUND + orderId);
+    }
+  }
 
   @Override
   @Transactional(rollbackOn = InventoryException.class)
