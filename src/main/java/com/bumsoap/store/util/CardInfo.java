aea5ee12644c20f53a9491bb885c8c7626db0e54
BumTiger <jbpark03@gmail.com>
2025-11-03 15:53:21 +0900

    card info - part of pay confirm response

---
+++ src/main/java/com/bumsoap/store/util/CardInfo.java
@@ -0,0 +1,20 @@
+package com.bumsoap.store.util;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class CardInfo {
+    private String cardOwnerType; // "개인
+    private String cardNumber; // "46731099****758*"
+    private BigDecimal cardAmount; // 10,700
+    private String cardType; // "체크"
+    private String cardIssuerCode; // "11"
+}
