455aa47f3bb4b3cd8a3a3077bc44b24f6739d897
BumTiger <jbpark03@gmail.com>
2025-11-08 08:58:39 +0900

    reads my orders list from DB - my version

--- src/main/java/com/bumsoap/store/repository/OrderRepo.java
+++ src/main/java/com/bumsoap/store/repository/OrderRepo.java
@@ -1,10 +1,25 @@
 package com.bumsoap.store.repository;
 
+import com.bumsoap.store.dto.MyOrderDto;
 import com.bumsoap.store.model.BsOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepo extends JpaRepository<BsOrder, Long> {
     Optional<BsOrder> findByOrderId(String orderId);
+
+    @Query(value = "select tp.approved_at as paymentTime, " +
+            "bo.order_id, bo.order_name, " +
+            "r.full_name as receipientName, " +
+            "bo.payment as paymentAmount," +
+            "tp.receipt_url  " +
+            "from bs_order bo, toss_payment tp, recipient r " +
+            "where bo.order_id = tp.order_id " +
+            "and bo.recipient_id = r.id " +
+            "order by approved_at desc ")
+    List<MyOrderDto> getRecentSome(@Param("count") int count);
 }
