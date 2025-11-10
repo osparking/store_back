779bec0a51781f5734b8879b7719ced2d53bccdf
BumTiger <jbpark03@gmail.com>
2025-11-10 11:33:17 +0900

    on DB, remove orphaned orders of an email

--- src/main/java/com/bumsoap/store/repository/OrderRepo.java
+++ src/main/java/com/bumsoap/store/repository/OrderRepo.java
@@ -2,12 +2,14 @@ package com.bumsoap.store.repository;
 
 import com.bumsoap.store.dto.MyOrderDto;
 import com.bumsoap.store.model.BsOrder;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepo extends JpaRepository<BsOrder, Long> {
@@ -29,4 +31,22 @@ public interface OrderRepo extends JpaRepository<BsOrder, Long> {
             """, nativeQuery = true)
     Page<MyOrderDto> findMyOrders(@Param("user_id") Long user_id,
                                   Pageable pageable);
+
+    @Query("""
+            SELECT bo
+            FROM BsOrder bo
+            LEFT JOIN bo.tossPayment tp
+            JOIN bo.user bu
+            WHERE bu.email = :email
+                AND tp IS NULL
+            """)
+    List<BsOrder> findOrdersByUserEmailWithoutPayments(String email);
+
+    @Transactional
+    default int deleteOrdersByUserEmailWithoutPayments(String email) {
+        List<BsOrder> ordersToDelete =
+                findOrdersByUserEmailWithoutPayments(email);
+        deleteAll(ordersToDelete);
+        return ordersToDelete.size();
+    }
 }
