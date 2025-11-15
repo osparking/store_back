a04b9b66312cc14e586cd848ed3ad0ab8ed08f52
BumTiger <jbpark03@gmail.com>
2025-11-15 22:29:16 +0900

    adds 2 members and convert ordinal to string

--- src/main/java/com/bumsoap/store/dto/RecipientDto.java
+++ src/main/java/com/bumsoap/store/dto/RecipientDto.java
@@ -1,16 +1,31 @@
 package com.bumsoap.store.dto;
 
+import com.bumsoap.store.util.DoroZbun;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@AllArgsConstructor
 @Getter
 @Setter
 public class RecipientDto {
     private String fullName;
     private String mbPhone;
     private String zipcode;
+    private String doroZbun;
     private String roadAddress;
+    private String zBunAddress;
     private String addressDetail;
+
+    public RecipientDto(String fullName, String mbPhone,
+                        String zipcode, int doroZbun,
+                        String roadAddress, String zBunAddress,
+                        String addressDetail) {
+        this.fullName = fullName;
+        this.mbPhone = mbPhone;
+        this.zipcode = zipcode;
+        this.doroZbun = DoroZbun.values()[doroZbun].toString();
+        this.roadAddress = roadAddress;
+        this.zBunAddress = zBunAddress;
+        this.addressDetail = addressDetail;
+    }
 }
