72aafc70758af0f49031c2ff08a6de6e0c656a4a
BumTiger <jbpark03@gmail.com>
2025-11-21 18:09:37 +0900

    order page row dto class constructor defined

--- src/main/java/com/bumsoap/store/dto/OrderPageRow.java
+++ src/main/java/com/bumsoap/store/dto/OrderPageRow.java
@@ -1,15 +1,17 @@
 package com.bumsoap.store.dto;
 
 import com.bumsoap.store.util.OrderStatus;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
-public class OrderTableRow {
+public class OrderPageRow {
   private String orderId;
   private LocalDateTime orderTime;
   private OrderStatus orderStatus;
@@ -18,6 +20,21 @@ public class OrderTableRow {
   private String recipient;
   private Long user_id;
   private BigDecimal payment;
+
+  public OrderPageRow(String orderId, Timestamp orderTime,
+                      int orderStatus, String orderName,
+                      String customer, String recipient,
+                      Long user_id, BigDecimal payment) {
+    this.orderId = orderId;
+    this.orderTime = orderTime.toLocalDateTime();
+    this.orderStatus = OrderStatus.values()[orderStatus];
+    this.orderName = orderName;
+    this.customer = customer;
+    this.recipient = recipient;
+    this.user_id = user_id;
+    this.payment = payment;
+  }
+
   public String getOrderStatus() {
     return orderStatus.label;
   }
