2dcc3be39d21fad315c1d67ce18f192d54c12409
BumTiger <jbpark03@gmail.com>
2025-11-27 07:10:38 +0900

    add status field to waybill store request

--- src/main/java/com/bumsoap/store/request/UpdateWaybillNoReq.java
+++ src/main/java/com/bumsoap/store/request/UpdateWaybillNoReq.java
@@ -1,11 +1,16 @@
 package com.bumsoap.store.request;
 
-import lombok.AllArgsConstructor;
+import com.bumsoap.store.util.OrderStatus;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class UpdateWaybillNoReq {
     private Long id;
     private String waybillNo;
+    private OrderStatus status;
+    public UpdateWaybillNoReq(Long id, String status, String waybillNo) {
+        this.id = id;
+        this.status = OrderStatus.valueOfLabel(status);
+        this.waybillNo = waybillNo;
+    }
 }
