b7db15d0ee4a2e95a3abded0a5b29dd98f08253c
BumTiger <jbpark03@gmail.com>
2025-12-05 14:16:34 +0900

    주문 상세 정보 채취 select 문 review 목표열 추가

--- src/main/java/com/bumsoap/store/repository/OrderRepo.java
+++ src/main/java/com/bumsoap/store/repository/OrderRepo.java
@@ -42,7 +42,7 @@ public interface OrderRepo extends JpaRepository<BsOrder, Long> {
     @Query(value = """
             select bo.id, bo.order_id, bo.order_time,
                     bo.order_status, bo.order_name,
-                    bu.full_name as customer,
+                    bo.review, bu.full_name as customer,
                     r.full_name as recipient,
                     bo.user_id, bo.payment, bo.waybill_no,
                     ab.zipcode, ab.road_address,
