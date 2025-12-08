84d0fcdfb19d22d2ab2b64f4ccd85a93b7b14782
BumTiger <jbpark03@gmail.com>
2025-12-08 18:39:00 +0900

    특정 후기 텍스트 등 포함하는 Dto

--- src/main/java/com/bumsoap/store/dto/ReviewInfo.java
+++ src/main/java/com/bumsoap/store/dto/ReviewInfo.java
@@ -1,4 +1,14 @@
 package com.bumsoap.store.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewInfo {
+    private String orderName;
+    private String review;
+    private Long userId;
 }
