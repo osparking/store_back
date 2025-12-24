a0582ede949215990d237b6b93b697288873cc62
BumTiger <jbpark03@gmail.com>
2025-12-24 20:59:07 +0900

    트래킹모어 서명 불일치 때 던질 예외

---
+++ src/main/java/com/bumsoap/store/exception/WrongSignatureException.java
@@ -0,0 +1,7 @@
+package com.bumsoap.store.exception;
+
+public class WrongSignatureException extends Exception{
+    public WrongSignatureException(String message) {
+        super(message);
+    }
+}
