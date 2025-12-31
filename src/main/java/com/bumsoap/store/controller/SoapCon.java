723ec6c4bcfb17f385cb23a854af97eca318c37b
BumTiger <jbpark03@gmail.com>
2026-01-01 05:11:27 +0900

    두 soap url 처리 종점 클래스 통합 및 멤버 개명

--- src/main/java/com/bumsoap/store/controller/SoapCon.java
+++ src/main/java/com/bumsoap/store/controller/SoapCon.java
@@ -1,25 +1,61 @@
 package com.bumsoap.store.controller;
 
+import com.bumsoap.store.dto.ShapeSelItem;
+import com.bumsoap.store.model.SoapInven;
+import com.bumsoap.store.request.InvenUpdateReq;
 import com.bumsoap.store.request.SoapPriceReq;
 import com.bumsoap.store.response.ApiResp;
 import com.bumsoap.store.service.order.OrderServI;
+import com.bumsoap.store.service.soap.InvenServI;
 import com.bumsoap.store.service.soap.PriceServI;
 import com.bumsoap.store.util.BsShape;
 import com.bumsoap.store.util.Feedback;
 import com.bumsoap.store.util.UrlMap;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RestController
 @RequestMapping(UrlMap.SOAP)
 @RequiredArgsConstructor
-public class SoapPriceCon {
+public class SoapCon {
+  private final InvenServI inventoryServ;
   private final PriceServI priceServ;
   private final OrderServI orderServ;
+
+  @GetMapping(UrlMap.SOAP_SHAPES)
+  public ResponseEntity<ApiResp> getShapeSelectionData() {
+    try {
+      List<ShapeSelItem> shapeSelItems = inventoryServ.getShapeSelItems();
+
+      return ResponseEntity.ok(
+              new ApiResp(Feedback.ORDER_FOUND, shapeSelItems));
+    } catch (Exception e) {
+      return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+              .body(new ApiResp(e.getMessage(), null));
+    }
+  }
+
+  @PutMapping(UrlMap.UPDATE)
+  public ResponseEntity<ApiResp> update(@RequestBody InvenUpdateReq request) {
+    try {
+      SoapInven soapInven = inventoryServ.getByBsShape(request.getBsShape());
+      soapInven.setStockLevel(
+              request.getStockDiff() + soapInven.getStockLevel());
+      SoapInven updatedSoap = inventoryServ.add(soapInven);
+      return ResponseEntity.ok(
+              new ApiResp(Feedback.INVEN_UPDATE_SUCC, updatedSoap));
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+              new ApiResp(e.getMessage(), null));
+    }
+  }
 
   @GetMapping(UrlMap.SOAP_PRICE)
   public ResponseEntity<ApiResp> getSoapPrice(@RequestBody SoapPriceReq request) {
