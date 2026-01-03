d7f3f72ef2d151d0faaaaaee731460b9bc3d1db5
BumTiger <jbpark03@gmail.com>
2026-01-03 20:36:27 +0900

    유저의 최근 6개월 주문 외형별 수량 질의

--- src/main/java/com/bumsoap/store/repository/OrderItemRepo.java
+++ src/main/java/com/bumsoap/store/repository/OrderItemRepo.java
@@ -1,12 +1,14 @@
 package com.bumsoap.store.repository;
 
 import com.bumsoap.store.dto.ItemsField;
+import com.bumsoap.store.dto.ShapeCountDTO;
 import com.bumsoap.store.model.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -17,4 +19,15 @@ public interface OrderItemRepo extends JpaRepository<OrderItem, Long> {
             where oi.order_id = :orderId;
             """, nativeQuery = true)
     List<ItemsField> findByOrderId(@Param("orderId") Long orderId);
+
+    @Query("SELECT oi.shape as shape, SUM(oi.count) as soaps " +
+            "FROM BsOrder bo " +
+            "JOIN bo.items oi " +
+            "WHERE bo.user.id = :userId " +
+            "AND bo.orderStatus IN (PURCHASE_CONFIRMED, REVIEWED) " +
+            "AND bo.orderTime >= :startDate " +
+            "GROUP BY oi.shape")
+    List<ShapeCountDTO> findSoapCountByShapeForUser(
+            @Param("userId") Long userId,
+            @Param("startDate") LocalDateTime startDate);
 }
