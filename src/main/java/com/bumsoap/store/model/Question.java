e93a8b0cc8768d6cf8815f10f51cabd2e0ce9d5f
BumTiger <jbpark03@gmail.com>
2026-01-06 07:25:44 +0900

    질문 저장 시각 포함, 요청값  초기화 생성자

--- src/main/java/com/bumsoap/store/model/Question.java
+++ src/main/java/com/bumsoap/store/model/Question.java
@@ -1,10 +1,12 @@
 package com.bumsoap.store.model;
 
+import com.bumsoap.store.request.QuestionSaveReq;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -23,6 +25,8 @@ public class Question {
     @Lob
     private String question;
 
+    @CreationTimestamp
+    @Column(name = "insert_time", updatable = false, nullable = false)
     private LocalDateTime insertTime;
     private LocalDateTime updateTime;
 
@@ -33,4 +37,13 @@ public class Question {
     @OneToOne
     @JoinColumn(name = "follow_up_id")
     private FollowUp followUp;
+
+    /**
+     * 제목 및 질문 내용 배정
+     * @param request
+     */
+    public Question(QuestionSaveReq request) {
+        this.title = request.getTitle();
+        this.question = request.getQuestion();
+    }
 }
