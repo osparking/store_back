17127b383465da7fe7ed12ac63ac1b4fb0583c9e
BumTiger <jbpark03@gmail.com>
2026-01-06 07:19:40 +0900

    질문 저장 요청 클래스 - 유저ID 등 3 멤버

---
+++ src/main/java/com/bumsoap/store/request/QuestionSaveReq.java
@@ -0,0 +1,12 @@
+package com.bumsoap.store.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class QuestionSaveReq {
+    private Long userId;
+    private String title;
+    private String question;
+}
