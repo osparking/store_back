5e5643621e428cdd63a91d24953d606b3e7ced2a
BumTiger <jbpark03@gmail.com>
2026-01-08 22:36:26 +0900

    질문 및 댓글 목록 읽는 저장소 메소드 시험 메소드

--- src/test/java/com/bumsoap/store/repository/QuestionRepoTest.java
+++ src/test/java/com/bumsoap/store/repository/QuestionRepoTest.java
@@ -1,4 +1,22 @@
+package com.bumsoap.store.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class QuestionRepoTest {
-  
+    @Autowired
+    private QuestionRepo questionRepo;
+
+    @Test
+    void findQuestionWithFollowUps() {
+        var question = questionRepo.findQuestionWithFollowUps(1L);
+        assertTrue(question.isPresent());
+        System.out.println(question.get().toString());
+    }
 }
