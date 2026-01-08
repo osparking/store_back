2fe98145abc60d9b750d0f90439855c6dace3f93
BumTiger <jbpark03@gmail.com>
2026-01-08 22:34:49 +0900

    질문과 그에 딸린 댓글의 목록을 채취

--- src/main/java/com/bumsoap/store/repository/QuestionRepo.java
+++ src/main/java/com/bumsoap/store/repository/QuestionRepo.java
@@ -2,6 +2,16 @@ package com.bumsoap.store.repository;
 
 import com.bumsoap.store.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface QuestionRepo extends JpaRepository<Question, Long> {
+    @Query("SELECT q FROM Question q " +
+            "LEFT JOIN FETCH q.followUps f " +
+            "WHERE q.id = :questionId " +
+            "ORDER BY f.insertTime DESC")
+    Optional<Question> findQuestionWithFollowUps(
+            @Param("questionId") Long questionId);
 }
