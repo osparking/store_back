dcb52f65b9df02efa7b6726f2d98bf271b9f9c4a
BumTiger <jbpark03@gmail.com>
2026-01-09 11:40:37 +0900

    관리자용 질문 테이블 행 목록 읽기 - 답변 여부

--- src/main/java/com/bumsoap/store/repository/QuestionRepo.java
+++ src/main/java/com/bumsoap/store/repository/QuestionRepo.java
@@ -1,10 +1,12 @@
 package com.bumsoap.store.repository;
 
+import com.bumsoap.store.dto.QuestionTableRowAdmin;
 import com.bumsoap.store.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface QuestionRepo extends JpaRepository<Question, Long> {
@@ -14,4 +16,30 @@ public interface QuestionRepo extends JpaRepository<Question, Long> {
             "ORDER BY f.insertTime DESC")
     Optional<Question> findQuestionWithFollowUps(
             @Param("questionId") Long questionId);
+
+    @Query(nativeQuery = true, value =
+            """
+            SELECT q.title, q.insert_time,
+            	   left(REGEXP_REPLACE(q.question, '<[^>]*>', ''), 30)
+            	   	 as question,
+            	   CASE
+                     WHEN latest_fu.user_id = 1 THEN '답변함'
+                     ELSE '미답변'
+                   END AS answered,
+                   latest_fu.user_id as last_writer_id,
+                   latest_fu.id as followUpId
+            FROM question q
+            LEFT OUTER JOIN (
+                SELECT f1.*
+                FROM follow_up f1
+                INNER JOIN (
+                    SELECT question_id, MAX(id) as max_id
+                    FROM follow_up
+                    GROUP BY question_id
+                ) f2 ON f1.question_id = f2.question_id and
+                        f1.id = f2.max_id
+            ) latest_fu ON q.id = latest_fu.question_id
+            """
+    )
+    List<QuestionTableRowAdmin> listQuestionTableRowForAdmin();
 }
