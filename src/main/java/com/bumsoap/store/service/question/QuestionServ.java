614ee919880f948eb9780901a152332b5d58f433
BumTiger <jbpark03@gmail.com>
2026-01-09 18:28:59 +0900

    나의 리뷰 목록 서비스 메소드와 일치화

--- src/main/java/com/bumsoap/store/service/question/QuestionServ.java
+++ src/main/java/com/bumsoap/store/service/question/QuestionServ.java
@@ -1,6 +1,7 @@
 package com.bumsoap.store.service.question;
 
 import com.bumsoap.store.dto.QuestionTableRowAdmin;
+import com.bumsoap.store.dto.SearchResult;
 import com.bumsoap.store.exception.IdNotFoundEx;
 import com.bumsoap.store.model.Question;
 import com.bumsoap.store.repository.QuestionRepo;
@@ -8,10 +9,13 @@ import com.bumsoap.store.repository.UserRepoI;
 import com.bumsoap.store.request.QuestionSaveReq;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service
 @RequiredArgsConstructor
@@ -20,10 +24,26 @@ public class QuestionServ implements QuestionServI {
     private final UserRepoI userRepo;
 
     @Override
-    public Page<QuestionTableRowAdmin> getQuestionsPage(Integer page,
-                                                        Integer size) {
-        Pageable pageable = PageRequest.of(page - 1, size);
-        return questionRepo.listQuestionTableRowForAdmin(pageable);
+    public SearchResult<QuestionTableRowAdmin> getQuestionsPage(Integer page,
+                                                                Integer pageSize) {
+        Pageable pageable = PageRequest.of(page - 1, pageSize);
+        var questionPage = questionRepo.listQuestionTableRowForAdmin(pageable);
+        int totalPages = questionPage.getTotalPages();
+
+        List<Integer> pageNumbers = null;
+
+        if (totalPages > 0) {
+            pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+        }
+
+        return new SearchResult<QuestionTableRowAdmin>(questionPage,
+                questionPage.getNumber() + 1,
+                pageSize,
+                totalPages,
+                pageNumbers
+        );
     }
 
     @Transactional
--- src/main/java/com/bumsoap/store/service/question/QuestionServI.java
+++ src/main/java/com/bumsoap/store/service/question/QuestionServI.java
@@ -1,13 +1,13 @@
 package com.bumsoap.store.service.question;
 
 import com.bumsoap.store.dto.QuestionTableRowAdmin;
+import com.bumsoap.store.dto.SearchResult;
 import com.bumsoap.store.model.Question;
 import com.bumsoap.store.request.QuestionSaveReq;
 import jakarta.transaction.Transactional;
-import org.springframework.data.domain.Page;
 
 public interface QuestionServI {
-    Page<QuestionTableRowAdmin> getQuestionsPage(Integer page, Integer size);
+    SearchResult<QuestionTableRowAdmin> getQuestionsPage(Integer page, Integer size);
 
     @Transactional
     Question handleSaveQuestion(QuestionSaveReq question);
