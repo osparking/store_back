ed87002d01bae7bf2171864a1e754eb53b6f4646
BumTiger <jbpark03@gmail.com>
2026-01-12 12:38:18 +0900

    질문 댓글 저장소 인터페이스

---
+++ src/main/java/com/bumsoap/store/repository/FollowUpRepo.java
@@ -0,0 +1,7 @@
+package com.bumsoap.store.repository;
+
+import com.bumsoap.store.model.FollowUp;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FollowUpRepo extends JpaRepository<FollowUp, Long> {
+}
