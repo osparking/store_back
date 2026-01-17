7b234b9af26cf1d91163003066cb97ca46d4623e
BumTiger <jbpark03@gmail.com>
2026-01-18 07:36:03 +0900

    이메일 전송 메소드 비동기적 실행 - 반응시간 단축목적

--- src/main/java/com/bumsoap/store/email/EmailManager.java
+++ src/main/java/com/bumsoap/store/email/EmailManager.java
@@ -1,22 +1,29 @@
 package com.bumsoap.store.email;
 
+import com.bumsoap.store.controller.QuestionCon;
 import jakarta.annotation.PostConstruct;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Properties;
+import java.util.concurrent.CompletableFuture;
 
 @Component
 public class EmailManager {
     private JavaMailSender sender;
+    private static final Logger logger =
+            LoggerFactory.getLogger(QuestionCon.class);
 
-    public void sendMail(String to, String subject, String senderName,
-                         String mailContent)
+    public void sendMailSync(String to, String subject, String senderName,
+                             String mailContent)
             throws MessagingException, UnsupportedEncodingException {
         MimeMessage message = sender.createMimeMessage();
         var messageHelper = new MimeMessageHelper(message);
@@ -25,6 +32,30 @@ public class EmailManager {
         messageHelper.setSubject(subject);
         messageHelper.setText(mailContent, true);
         sender.send(message);
+    }
+
+    @Async("emailTaskExecutor") // 특정 executor 지정 가능
+    public CompletableFuture<Void> sendMail(String to,
+                                            String subject,
+                                            String senderName,
+                                            String mailContent)
+            throws MessagingException, UnsupportedEncodingException {
+        long start = System.currentTimeMillis();
+        try {
+            // 기존 이메일 전송 로직
+            MimeMessage message = sender.createMimeMessage();
+            var messageHelper = new MimeMessageHelper(message);
+            messageHelper.setFrom(EmailProperties.DEFAULT_USERNAME, senderName);
+            messageHelper.setTo(to);
+            messageHelper.setSubject(subject);
+            messageHelper.setText(mailContent, true);
+            sender.send(message);
+            logger.info("이메일 전송 완료: {}ms", System.currentTimeMillis() - start);
+            return CompletableFuture.completedFuture(null);
+        } catch (Exception e) {
+            logger.error("이메일 전송 실패: {}", e.getMessage(), e);
+            return CompletableFuture.failedFuture(e);
+        }
     }
 
     private JavaMailSender createMailSender() {
