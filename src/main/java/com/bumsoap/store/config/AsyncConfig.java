0b2af10b3ddc908032064534c5c48852bec1410f
BumTiger <jbpark03@gmail.com>
2026-01-18 07:29:33 +0900

    이메일 전송 메소드 비동기 실행을 위한 실행기

---
+++ src/main/java/com/bumsoap/store/config/AsyncConfig.java
@@ -0,0 +1,23 @@
+package com.bumsoap.store.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "emailTaskExecutor")
+    public AsyncTaskExecutor emailTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5); // 기본 스레드 수
+        executor.setMaxPoolSize(10); // 최대 스레드 수
+        executor.setQueueCapacity(100); // 대기 큐 크기
+        executor.setThreadNamePrefix("email-async-");
+        executor.initialize();
+        return executor;
+    }
+}
