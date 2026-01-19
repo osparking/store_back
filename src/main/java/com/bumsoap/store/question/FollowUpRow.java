32fb644f1ae5712b284d0c3529e6b0d98c9341be
BumTiger <jbpark03@gmail.com>
2026-01-19 18:53:48 +0900

    댓글 작성자가 관리자(bum)인지 여부 표시

--- src/main/java/com/bumsoap/store/question/FollowUpRow.java
+++ src/main/java/com/bumsoap/store/question/FollowUpRow.java
@@ -9,9 +9,11 @@ import lombok.NoArgsConstructor;
 public class FollowUpRow {
     private long id;
     private String content;
+    private boolean bumWrote;
 
     public FollowUpRow(FollowUp followUp) {
         this.id = followUp.getId();
         this.content = followUp.getContent();
+        this.bumWrote = followUp.getUser().getId() == 1L;
     }
 }
