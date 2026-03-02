dee56691b5afbbe57dfb07f6cef1068f30ad05cd
BumTiger <jbpark03@gmail.com>
2026-03-02 13:37:35 +0900

    재료 페이지 전부 채취 서비스 메소드

--- src/main/java/com/bumsoap/store/service/store/StoreIngreServ.java
+++ src/main/java/com/bumsoap/store/service/store/StoreIngreServ.java
@@ -1,11 +1,22 @@
 package com.bumsoap.store.service.store;
 
+import com.bumsoap.store.dto.*;
 import com.bumsoap.store.exception.IdNotFoundEx;
 import com.bumsoap.store.model.StoreIngre;
 import com.bumsoap.store.repository.StoreIngreRepoI;
+import com.bumsoap.store.util.BsParameters;
 import com.bumsoap.store.util.Feedback;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service
 @RequiredArgsConstructor
@@ -29,6 +40,44 @@ public class StoreIngreServ implements StoreIngreServI {
     } else {
       throw new IdNotFoundEx(Feedback.ST_RE_ID_NOT_FOUND + id);
     }
+  }
+
+  @Autowired
+  private BsParameters provider;
+
+  private final EntityConverter<StoreIngreRow, StoreIngreDto> stInConverter;
+
+  @Override
+  public SearchResult<StoreIngreDto> getIngredientPage(String name,
+                                  Optional<Integer> page,
+                                  Optional<Integer> size) {
+    int pageSize = size.orElse(provider.getPageSize());
+    Pageable pageable = PageRequest.of(page.orElse(1) - 1, pageSize);
+    Page<StoreIngreRow> ingredientPage = null;
+
+    if (name == null || "".equals(name)) {
+      ingredientPage = storeIngreRepo.findPageAll(pageable);
+    } else {
+//      ingredientPage = storeIngreRepo.findPageByName(name, pageable);
+    }
+    Page<StoreIngreDto> ingredientDtos = ingredientPage.map(row
+            -> stInConverter.mapEntityToDto(row, StoreIngreDto.class));
+    int totalPages = ingredientPage.getTotalPages();
+    List<Integer> pageNumbers = null;
+
+    if (totalPages > 0) {
+      pageNumbers = IntStream.rangeClosed(1, totalPages)
+              .boxed()
+              .collect(Collectors.toList());
+    }
+
+    var result = new SearchResult<StoreIngreDto>(ingredientDtos,
+            ingredientPage.getNumber() + 1,
+            pageSize,
+            totalPages,
+            pageNumbers
+    );
+    return result;
   }
 
 }
--- src/main/java/com/bumsoap/store/service/store/StoreIngreServI.java
+++ src/main/java/com/bumsoap/store/service/store/StoreIngreServI.java
@@ -2,8 +2,13 @@ package com.bumsoap.store.service.store;
 
 import com.bumsoap.store.model.StoreIngre;
 
+import java.util.Optional;
+
 public interface StoreIngreServI {
   StoreIngre findById(Long id);
 
   String deleteById(Long id);
+
+  Object getIngredientPage(String name, Optional<Integer> page,
+                           Optional<Integer> size);
 }
