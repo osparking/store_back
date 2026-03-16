a7637d26e660836c09e87a9f715f2287aeeb0bac
BumTiger <jbpark03@gmail.com>
2026-03-16 09:09:04 +0900

    부서별 인원 질의 결과용 DTO

---
+++ src/main/java/com/bumsoap/store/dto/PeopleByDept.java
@@ -0,0 +1,13 @@
+package com.bumsoap.store.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PeopleByDept {
+    private String department;
+    private Long people;
+}
