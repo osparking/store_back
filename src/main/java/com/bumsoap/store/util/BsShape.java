b8096c9923073360dc2f7dff11cbdbbf89cbc204
BumTiger <jbpark03@gmail.com>
2026-03-22 11:20:27 +0900

    월별키 생성 함수 - 맴에 항목 저장 및 채취용

--- src/main/java/com/bumsoap/store/util/BsShape.java
+++ src/main/java/com/bumsoap/store/util/BsShape.java
@@ -1,27 +1,31 @@
 package com.bumsoap.store.util;
 
 public enum BsShape {
-  NORMAL("보통비누"),
-  S_WHITE("백설공주"),
-  MAEJU_S("메주비누");
+    NORMAL("보통비누"),
+    S_WHITE("백설공주"),
+    MAEJU_S("메주비누");
 
-  public final String label;
+    public final String label;
 
-  public static BsShape valueOfLabel(String label) {
-    for (BsShape e : values()) {
-      if (e.label.equals(label)) {
-        return e;
-      }
+    public String getKeyForMonth(String month) {
+        return month + this.ordinal();
     }
-    return null;
-  }
 
-  private BsShape(String label) {
-    this.label = label;
-  }
+    public static BsShape valueOfLabel(String label) {
+        for (BsShape e : values()) {
+            if (e.label.equals(label)) {
+                return e;
+            }
+        }
+        return null;
+    }
 
-  @Override
-  public String toString() {
-    return label;
-  }
+    private BsShape(String label) {
+        this.label = label;
+    }
+
+    @Override
+    public String toString() {
+        return label;
+    }
 }
