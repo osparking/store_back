64f485041c3a4a46e60fa013ded5882924d20e45
BumTiger <jbpark03@gmail.com>
2026-03-23 10:04:05 +0900

    비누 외형별 생산 기록 저장 테이블 클래스

---
+++ src/main/java/com/bumsoap/store/model/SoapProduce.java
@@ -0,0 +1,30 @@
+package com.bumsoap.store.model;
+
+import com.bumsoap.store.util.BsShape;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class SoapProduce {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BsShape bsShape;
+    private LocalDate produceDate;
+    private Long producerId; // 생산직원 ID
+    private Long registerId; // 등록직원 ID
+
+    @Column(name = "add_time", updatable = false, nullable = false,
+            columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime registerTime = LocalDateTime.now(); // 등록 시간
+}
