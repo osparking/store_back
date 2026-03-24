3054b9d7a839929f4b3371c437e4e2d922de72e9
BumTiger <jbpark03@gmail.com>
2026-03-24 14:05:15 +0900

    비누 생산 정보 페이지 - 저장소 JPA 질의문 메소드

--- src/main/java/com/bumsoap/store/repository/ProduceRepo.java
+++ src/main/java/com/bumsoap/store/repository/ProduceRepo.java
@@ -1,9 +1,25 @@
 package com.bumsoap.store.repository;
 
+import com.bumsoap.store.dto.ProducePageRow;
 import com.bumsoap.store.model.SoapProduce;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProduceRepo extends JpaRepository<SoapProduce, Long> {
+    @Query("""
+            select new com.bumsoap.store.dto.ProducePageRow(
+                sp.bsShape, sp.quantity, sp.produceDate,
+                bu.fullName as producer,
+                bu2.fullName as register, sp.registerTime)
+            from SoapProduce sp
+            join BsUser bu on bu.id = sp.producerId
+            join BsUser bu2 on bu2.id = sp.registerId
+            order by sp.produceDate desc, sp.registerTime desc
+            """
+    )
+    Page<ProducePageRow> getSoapProducePage(Pageable pageable);
 }
