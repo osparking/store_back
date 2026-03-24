7b2d3d5e73700e5894589c91a56fb03ced69ffd2
BumTiger <jbpark03@gmail.com>
2026-03-24 14:56:40 +0900

    생산 정보 페이지 반환 서비스: Row > Dto 변환 포함

--- src/main/java/com/bumsoap/store/service/produce/ProduceServ.java
+++ src/main/java/com/bumsoap/store/service/produce/ProduceServ.java
@@ -1,10 +1,18 @@
 package com.bumsoap.store.service.produce;
 
+import com.bumsoap.store.dto.ProduceDto;
+import com.bumsoap.store.dto.SearchResult;
 import com.bumsoap.store.model.SoapProduce;
 import com.bumsoap.store.repository.ProduceRepo;
 import com.bumsoap.store.request.AddProduceReq;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +26,27 @@ public class ProduceServ implements ProduceServI {
 
         soapProduce.setRegisterId(registerWorkerId);
         return produceRepo.save(soapProduce);
+    }
+
+    @Override
+    public SearchResult<ProduceDto> getProducePage(
+            Integer page, Integer size) {
+
+        Pageable pageable = PageRequest.of(page - 1, size);
+        var produceRowPage = produceRepo.getSoapProducePage(pageable);
+        var dtoPage = produceRowPage.map(ProduceDto::new);
+
+        int totalPages = produceRowPage.getTotalPages();
+        List<Integer> pageNumbers = null;
+
+        if (totalPages > 0) {
+            pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+        }
+
+        return new SearchResult<>(dtoPage, produceRowPage.getNumber() + 1,
+                size, totalPages, pageNumbers
+        );
     }
 }
--- src/main/java/com/bumsoap/store/service/produce/ProduceServI.java
+++ src/main/java/com/bumsoap/store/service/produce/ProduceServI.java
@@ -1,9 +1,14 @@
 package com.bumsoap.store.service.produce;
 
+import com.bumsoap.store.dto.ProduceDto;
+import com.bumsoap.store.dto.SearchResult;
 import com.bumsoap.store.model.SoapProduce;
 import com.bumsoap.store.request.AddProduceReq;
 
 public interface ProduceServI {
     SoapProduce addProduce(
             Long registerWorkerId, AddProduceReq request);
+
+    SearchResult<ProduceDto> getProducePage(Integer page,
+                                            Integer size);
 }
