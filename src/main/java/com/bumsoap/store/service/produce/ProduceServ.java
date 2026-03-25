2961ade1a63aed68840ccf7ba8c4ebce3b7e6dc0
BumTiger <jbpark03@gmail.com>
2026-03-25 14:58:35 +0900

    생성 레코드 삭제 - 일단, ID로 읽고, 객체를 삭제

--- src/main/java/com/bumsoap/store/service/produce/ProduceServ.java
+++ src/main/java/com/bumsoap/store/service/produce/ProduceServ.java
@@ -2,9 +2,11 @@ package com.bumsoap.store.service.produce;
 
 import com.bumsoap.store.dto.ProduceDto;
 import com.bumsoap.store.dto.SearchResult;
+import com.bumsoap.store.exception.IdNotFoundEx;
 import com.bumsoap.store.model.SoapProduce;
 import com.bumsoap.store.repository.ProduceRepo;
 import com.bumsoap.store.request.AddProduceReq;
+import com.bumsoap.store.util.Feedback;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -13,6 +15,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import static com.bumsoap.store.util.BsUtils.getShortTimeStr;
 
 @Service
 @RequiredArgsConstructor
@@ -50,8 +54,22 @@ public class ProduceServ implements ProduceServI {
         );
     }
 
+    /**
+     * 생성된 비누 외형-수량 정보 레코드를 아이디로 검색하여 삭제
+     * @param id : 삭제 대상 비누 생성 레코드 아이디
+     * @return : 삭제된 생성 레코드 지역 등록 시간 짧은 형태 문자열
+     */
     @Override
     public String deleteById(Long id) {
-        return "백설공주";
+        var produceToDelete = produceRepo.findById(id);
+
+        if (produceToDelete.isPresent()) {
+            var storedProduce = produceToDelete.get();
+
+            produceRepo.delete(storedProduce);
+            return getShortTimeStr(storedProduce.getRegisterTime());
+        } else {
+            throw new IdNotFoundEx(Feedback.ST_RE_ID_NOT_FOUND + id);
+        }
     }
 }
