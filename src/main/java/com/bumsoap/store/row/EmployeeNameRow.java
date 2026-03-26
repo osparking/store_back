7f8076324156be91e3c6e6265273bf2e946f43d6
BumTiger <jbpark03@gmail.com>
2026-03-26 20:47:45 +0900

    직원명 페이지 질의 결과 저장용 Row 클래스 정의

---
+++ src/main/java/com/bumsoap/store/row/EmployeeNameRow.java
@@ -0,0 +1,11 @@
+package com.bumsoap.store.row;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class EmployeeNameRow {
+    private Long id;
+    private String uniqName;
+}
