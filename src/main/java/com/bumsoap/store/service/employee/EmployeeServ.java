cfccdeed48825d4cd7b2ddc2f2225b9113bfdbb4
BumTiger <jbpark03@gmail.com>
2026-03-26 20:56:49 +0900

    직원명 페이지 질의 결과 페이지 서비스

--- src/main/java/com/bumsoap/store/service/employee/EmployeeServ.java
+++ src/main/java/com/bumsoap/store/service/employee/EmployeeServ.java
@@ -1,16 +1,27 @@
 package com.bumsoap.store.service.employee;
 
+import com.bumsoap.store.dto.SearchResult;
 import com.bumsoap.store.exception.IdNotFoundEx;
 import com.bumsoap.store.model.Employee;
 import com.bumsoap.store.repository.EmployeeRepoI;
+import com.bumsoap.store.repository.UserRepoI;
+import com.bumsoap.store.row.EmployeeNameRow;
 import com.bumsoap.store.util.Feedback;
+import com.bumsoap.store.util.UserType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service
 @RequiredArgsConstructor
 public class EmployeeServ implements EmployeeServInt{
     private final EmployeeRepoI employeeRepo;
+    private final UserRepoI userRepoI;
 
     @Override
     public Employee findById(Long empId) {
@@ -21,5 +32,27 @@ public class EmployeeServ implements EmployeeServInt{
     @Override
     public Employee save(Employee employee) {
         return employeeRepo.save(employee);
+    }
+
+    @Override
+    public SearchResult<EmployeeNameRow> getEmpNamesPage(
+            Integer page, Integer size, String name) {
+
+        Pageable pageable = PageRequest.of(page - 1, size);
+        var employeeNamePage = userRepoI.getEmployeeNames(
+                UserType.WORKER.ordinal(), name, pageable);
+        int totalPages = employeeNamePage.getTotalPages();
+        List<Integer> pageNumbers = null;
+
+        if (totalPages > 0) {
+            pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+        }
+
+        return new SearchResult<>(employeeNamePage,
+                employeeNamePage.getNumber() + 1,
+                size, totalPages, pageNumbers
+        );
     }
 }
--- src/main/java/com/bumsoap/store/service/employee/EmployeeServInt.java
+++ src/main/java/com/bumsoap/store/service/employee/EmployeeServInt.java
@@ -1,8 +1,13 @@
 package com.bumsoap.store.service.employee;
 
+import com.bumsoap.store.dto.SearchResult;
 import com.bumsoap.store.model.Employee;
+import com.bumsoap.store.row.EmployeeNameRow;
 
 public interface EmployeeServInt {
     Employee findById(Long empId);
     Employee save(Employee employee);
+
+    SearchResult<EmployeeNameRow> getEmpNamesPage(
+            Integer page, Integer size, String name);
 }
