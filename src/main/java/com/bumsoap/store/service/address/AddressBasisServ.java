94e4b008c8937b28eb670c33dbe97cbe8a29d788
BumTiger <jbpark03@gmail.com>
2026-05-24 10:59:44 +0900

    주석 위치 변경: 클래스에서 인터페이스로.

--- src/main/java/com/bumsoap/store/service/address/AddressBasisServ.java
+++ src/main/java/com/bumsoap/store/service/address/AddressBasisServ.java
@@ -23,13 +23,6 @@ public class AddressBasisServ implements AddressBasisServI {
         return addrBasisRepo.findByRoadAddress(roadAddress).orElse(null);
     }
 
-    /**
-     * 추가하려는 기초 주소가 이미 DB 에 있으면, 그 것을,
-     * 아니면(없으면), 삽입한 뒤 그 반환값을 돌려준다.
-     *
-     * @param addressBasis 삽입하려는 기초 주소
-     * @return (결과적으로) DB에 존재하는 기초 주소
-     */
     @Override
     public AddressBasis addGetAddrBasis(AddressBasis addressBasis) {
         // 1. 먼저 조회
--- src/main/java/com/bumsoap/store/service/address/AddressBasisServI.java
+++ src/main/java/com/bumsoap/store/service/address/AddressBasisServI.java
@@ -5,5 +5,13 @@ import com.bumsoap.store.model.AddressBasis;
 public interface AddressBasisServI {
   AddressBasis saveUpdate(AddressBasis addressBasis);
   AddressBasis findByRoadAddress(String roadAddress);
+
+  /**
+   * 추가하려는 기초 주소가 이미 DB 에 있으면, 그 것을,
+   * 아니면(없으면), 삽입한 뒤 그 반환값을 돌려준다.
+   *
+   * @param addressBasis 삽입하려는 기초 주소
+   * @return (결과적으로) DB에 존재하는 기초 주소
+   */
   AddressBasis addGetAddrBasis(AddressBasis addressBasis);
 }
