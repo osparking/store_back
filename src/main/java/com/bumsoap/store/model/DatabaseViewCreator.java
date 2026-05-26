18219d68b4b30c2908c89f93871bb62488cd19e4
BumTiger <jbpark03@gmail.com>
2026-05-26 15:06:54 +0900

    고아 수신처 ID 로 삭제하는 디비 프로시져

--- src/main/java/com/bumsoap/store/model/DatabaseViewCreator.java
+++ src/main/java/com/bumsoap/store/model/DatabaseViewCreator.java
@@ -37,4 +37,35 @@ public class DatabaseViewCreator {
             System.err.println("Failed to create view: " + e.getMessage());
         }
     }
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void createOrphanRecipientDeleteProcedure() {
+        String dropSql =
+                "DROP PROCEDURE IF EXISTS safe_delete_recipient";
+        String createSql =
+                """
+                CREATE PROCEDURE bs_store.safe_delete_recipient(IN re_id INT)
+                BEGIN
+                    DECLARE order_exists INT;
+                    -- 주문 존재 여부 확인
+                    SELECT COUNT(*) INTO order_exists
+                    FROM bs_order
+                    WHERE recipient_id = re_id;
+                    -- 주문이 없을 때만 삭제
+                    IF order_exists = 0 THEN
+                        DELETE FROM recipient WHERE id = re_id;
+                        SELECT CONCAT('삭제된 고아 수신처 ID : ', re_id) AS message;
+                    ELSE
+                        SELECT CONCAT('유지 필요한 수신처 ID : ', re_id) AS message;
+                    END IF;
+                END
+                """;
+        try {
+            jdbcTemplate.execute(dropSql);
+            jdbcTemplate.execute(createSql);
+            System.out.println("프로시저 'safe_delete_recipient' 생성 성공");
+        } catch (Exception e) {
+            System.err.println("생성이 실패된 프로시져 : " + e.getMessage());
+        }
+    }
 }
