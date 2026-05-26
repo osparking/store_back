87687820a171dc9301fc5ad74faab995d5a12340
BumTiger <jbpark03@gmail.com>
2026-05-26 16:41:47 +0900

    수신처 저장소 I' 고아 수신처 제거 메소드

--- src/main/java/com/bumsoap/store/repository/RecipientRepoI.java
+++ src/main/java/com/bumsoap/store/repository/RecipientRepoI.java
@@ -8,18 +8,21 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface RecipientRepoI extends JpaRepository<Recipient, Long> {
-  @Query(nativeQuery = true, value =
-      "select r.* from recipient r where " +
-          "r.addr_basis_id = :addr_basis_id " +
-          "and r.address_detail = :address_detail " +
-          "and r.doro_zbun = :doro_zbun " +
-          "and r.full_name = :full_name " +
-          "and r.mb_phone = :mb_phone " +
-          "limit 1")
-  Optional<Recipient> getIdenticalRecipient(
-      @Param("addr_basis_id") Long addr_basis_id,
-      @Param("address_detail") String address_detail,
-      @Param("doro_zbun") int doro_zbun,
-      @Param("full_name") String full_name,
-      @Param("mb_phone") String mb_phone);
+    @Query(nativeQuery = true, value =
+            "select r.* from recipient r where " +
+                    "r.addr_basis_id = :addr_basis_id " +
+                    "and r.address_detail = :address_detail " +
+                    "and r.doro_zbun = :doro_zbun " +
+                    "and r.full_name = :full_name " +
+                    "and r.mb_phone = :mb_phone " +
+                    "limit 1")
+    Optional<Recipient> getIdenticalRecipient(
+            @Param("addr_basis_id") Long addr_basis_id,
+            @Param("address_detail") String address_detail,
+            @Param("doro_zbun") int doro_zbun,
+            @Param("full_name") String full_name,
+            @Param("mb_phone") String mb_phone);
+
+    @Query(value = "CALL safe_delete_recipient(:re_id)", nativeQuery = true)
+    String safeDeleteRecipient(Long re_id);
 }
