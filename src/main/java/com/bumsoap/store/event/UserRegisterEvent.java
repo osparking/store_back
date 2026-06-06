31e4894a093ad7b8475ed6e8b9386a9d81975f36
BumTiger <jbpark03@gmail.com>
2026-06-06 12:56:03 +0900

    사용자 등록 사건에 검증코드도 실어 보냄

--- src/main/java/com/bumsoap/store/event/UserRegisterEvent.java
+++ src/main/java/com/bumsoap/store/event/UserRegisterEvent.java
@@ -8,9 +8,11 @@ import org.springframework.context.ApplicationEvent;
 @Getter @Setter
 public class UserRegisterEvent extends ApplicationEvent {
     private BsUser user;
+    private String verificationCode;
 
-    public UserRegisterEvent(BsUser user) {
+    public UserRegisterEvent(BsUser user, String verificationCode) {
         super(user);
         this.user = user;
+        this.verificationCode = verificationCode;
     }
 }
