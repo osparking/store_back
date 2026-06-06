48aae735e2c98a1acf4bae0e165523914d9ccde0
BumTiger <jbpark03@gmail.com>
2026-06-06 16:37:34 +0900

    만료된 토큰의 경우 토큰 재발급 결과를 열거항목화

--- src/main/java/com/bumsoap/store/util/TokenResult.java
+++ src/main/java/com/bumsoap/store/util/TokenResult.java
@@ -4,7 +4,8 @@ public enum TokenResult {
     INVALID("잘못된 토큰"),
     VERIFIED("이미 검증된 토큰"),
     EXPIRED("만료된 토큰"),
-    VALIDATED("계정 활성화");
+    VALIDATED("계정 활성화"),
+    REISSUED("토큰 재발급됨");
 
     public final String label;
 
