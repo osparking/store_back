27e301e901a0696042cd666eda8139069814071f
BumTiger <jbpark03@gmail.com>
2026-06-09 11:56:16 +0900

    폐쇄 계정 활성화 - 요청자료, 허용 URL

--- src/main/java/com/bumsoap/store/config/AppSecurityConfig.java
+++ src/main/java/com/bumsoap/store/config/AppSecurityConfig.java
@@ -91,6 +91,7 @@ public class AppSecurityConfig {
 
     private static String[] PRRMIT_URLS = {
             "/api/s1/user/add",
+            "/api/s1/user/enable",
             "/api/s1/worker/get_all_dept"
     };
 
---
+++ src/main/java/com/bumsoap/store/request/EnableUserReq.java
@@ -0,0 +1,8 @@
+package com.bumsoap.store.request;
+
+import lombok.Data;
+
+@Data
+public class EnableUserReq {
+    private String email;
+}
