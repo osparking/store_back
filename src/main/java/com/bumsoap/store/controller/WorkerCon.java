b5796afad9f0507dc5ca9ae0ee6dd4a382ec770b
BumTiger <jbpark03@gmail.com>
2026-06-18 16:27:01 +0900

    직원 모의 삭제 요청 종점 및 피드백 메시지

--- src/main/java/com/bumsoap/store/controller/WorkerCon.java
+++ src/main/java/com/bumsoap/store/controller/WorkerCon.java
@@ -3,11 +3,15 @@ package com.bumsoap.store.controller;
 import com.bumsoap.store.request.DeptChangeReq;
 import com.bumsoap.store.response.ApiResp;
 import com.bumsoap.store.service.worker.WorkerServInt;
+import com.bumsoap.store.util.BsUtils;
 import com.bumsoap.store.util.Feedback;
 import com.bumsoap.store.util.UrlMap;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @RestController
 @RequestMapping(UrlMap.WORKER)
@@ -41,6 +45,27 @@ public class WorkerCon {
         } catch (Exception e) {
             return ResponseEntity.internalServerError().body(
                     new ApiResp(Feedback.DEPTS_READ_FAILURE, e.getMessage()));
+        }
+    }
+
+    @DeleteMapping(UrlMap.DELETE_BY_ID)
+    public ResponseEntity<ApiResp> setAsDeleted(@PathVariable("id") Long id) {
+        try {
+            if (BsUtils.isQualified(id, false, null)) {
+                int deletedCount = workerServ.setWorkerDeleted(id);
+                if (deletedCount==1) {
+                    return ResponseEntity.ok(new ApiResp(
+                            Feedback.DELETEED_WORKER_ID + id, null));
+                } else {
+                    throw new RuntimeException("삭제 실패 직원 ID: " + id);
+                }
+            } else {
+                return ResponseEntity.status(UNAUTHORIZED).body(
+                        new ApiResp(Feedback.NOT_QUALIFIED_FOR + id, null));
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(NOT_FOUND)
+                    .body(new ApiResp(e.getMessage(), null));
         }
     }
 }
--- src/main/java/com/bumsoap/store/util/Feedback.java
+++ src/main/java/com/bumsoap/store/util/Feedback.java
@@ -14,6 +14,7 @@ public class Feedback {
     public static final String USER_ID_NOT_FOUND = "발견되지 않은 유저 아이디:";
     public static final String USER_UPDATE_SUCCESS = "유저 갱신 성공 - [닫기]로 마감하세요.";
     public static final String DELETEED_USER_NAME = "성공적으로 삭제된 유저 성명: ";
+    public static final String DELETEED_WORKER_ID = "성공적으로 삭제된 직원 ID: ";
     public static final String DISABLED_USER_NAME = "성공적으로 폐쇄된 유저 성명: ";
     public static final String USER_DTO_BY_ID = "ID로 유저 찾기 성공";
     public static final String DEPTS_FOUND = "소속 목록 조회됨";
