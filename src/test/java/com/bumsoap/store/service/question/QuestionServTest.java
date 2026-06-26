240f738358fca4474b767743d4a0af4ed1c83100
BumTiger <jbpark03@gmail.com>
2026-06-27 08:22:12 +0900

    질문 및 저자 성명 찾는 메소드 시험 클래스

--- src/test/java/com/bumsoap/store/service/question/QuestionServTest.java
+++ src/test/java/com/bumsoap/store/service/question/QuestionServTest.java
@@ -1,4 +1,30 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.bumsoap.store.service.question;
+
+import com.bumsoap.store.repository.QuestionRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class QuestionServTest {
-  
+
+    @Autowired
+    private QuestionRepo questionRepo;
+
+    @Autowired
+    private QuestionServI questionServ;
+
+    @Test
+    void findWriterNameByQuestionId() {
+        var question = questionRepo.findById(1L);
+        assert question.isEmpty() ||
+                (!question.get().getUser().getFullName().isEmpty());
+    }
+
+    @Test
+    void findWriterNameByQuestionIdServ() {
+        var writerName = questionServ.findWriterNameByQuestionId(1L);
+        System.out.println(writerName);
+        assert (!writerName.isEmpty());
+    }
 }
