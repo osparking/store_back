0e157a19b69c9583f75278959693e3f066123ade
BumTiger <jbpark03@gmail.com>
2026-06-27 09:23:16 +0900

    댓글 등록 사실 통지 이메일 수신자 정보 2 항목

--- src/main/java/com/bumsoap/store/dto/FollowUpDto.java
+++ src/main/java/com/bumsoap/store/dto/FollowUpDto.java
@@ -14,4 +14,14 @@ public class FollowUpDto {
     private String followUpContent;
     private LocalDateTime insertTime;
     private LocalDateTime updateTime;
+    private String senderName;
+    /**
+     * 댓글 등록 사실 통지 이메일을 수신자(관리자 혹은 질문 작성자) 이메일
+     */
+    private String receiverEmail;
+
+    /**
+     * 댓글 등록 사실 통지 이메일을 수신자(관리자 혹은 질문 작성자) 성명
+     */
+    private String receiverName;
 }
