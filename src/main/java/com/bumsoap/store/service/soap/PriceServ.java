a6e8af226cd73a98337c586d40ade5610555f579
BumTiger <jbpark03@gmail.com>
2026-06-28 16:44:26 +0900

    비누외형 3종 라벨별 가격 서비스

--- src/main/java/com/bumsoap/store/service/soap/PriceServ.java
+++ src/main/java/com/bumsoap/store/service/soap/PriceServ.java
@@ -2,24 +2,36 @@ package com.bumsoap.store.service.soap;
 
 import com.bumsoap.store.model.SoapPrice;
 import com.bumsoap.store.repository.SoapPriceRepo;
+import com.bumsoap.store.row.SoapPriceRow;
 import com.bumsoap.store.util.BsShape;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class PriceServ implements PriceServI{
-  private final SoapPriceRepo soapRepo;
+public class PriceServ implements PriceServI {
+    private final SoapPriceRepo soapRepo;
 
-  @Override
-  public BigDecimal findSoapPrice(BsShape bsShape) {
-    return soapRepo.findSoapPrice(bsShape.ordinal());
-  }
+    @Override
+    public BigDecimal findSoapPrice(BsShape bsShape) {
+        return soapRepo.findSoapPrice(bsShape.ordinal());
+    }
 
-  @Override
-  public SoapPrice add(SoapPrice soapInven) {
-    return soapRepo.save(soapInven);
-  }
+    @Override
+    public List<SoapPriceRow> findSoapPrices() {
+        var priceDtoList = soapRepo.findSoapPrices();
+
+        return priceDtoList.stream()
+                .map(SoapPriceRow::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public SoapPrice add(SoapPrice soapInven) {
+        return soapRepo.save(soapInven);
+    }
 }
--- src/main/java/com/bumsoap/store/service/soap/PriceServI.java
+++ src/main/java/com/bumsoap/store/service/soap/PriceServI.java
@@ -1,12 +1,16 @@
 package com.bumsoap.store.service.soap;
 
 import com.bumsoap.store.model.SoapPrice;
+import com.bumsoap.store.row.SoapPriceRow;
 import com.bumsoap.store.util.BsShape;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public interface PriceServI {
   BigDecimal findSoapPrice(BsShape bsShape);
+
+  List<SoapPriceRow> findSoapPrices();
 
   SoapPrice add(SoapPrice soapInven);
 }
