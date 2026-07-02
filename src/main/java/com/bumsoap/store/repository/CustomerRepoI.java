2b013f429827bacc3fd260c68cf5ed4af1a22ed3
BumTiger <jbpark03@gmail.com>
2026-07-02 14:08:03 +0900

    고객 레코드 페이지 채취 저장소 메소드

--- src/main/java/com/bumsoap/store/repository/CustomerRepoI.java
+++ src/main/java/com/bumsoap/store/repository/CustomerRepoI.java
@@ -1,9 +1,14 @@
 package com.bumsoap.store.repository;
 
 import com.bumsoap.store.model.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepoI extends JpaRepository<Customer, Long> {
+    @Query("SELECT c FROM Customer c")
+    Page<Customer> getOnePage(Pageable pageable);
 }
