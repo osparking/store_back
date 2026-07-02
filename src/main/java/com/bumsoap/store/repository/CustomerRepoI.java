2110239968929205de052b34deff2aefede1013e
BumTiger <jbpark03@gmail.com>
2026-07-02 17:55:32 +0900

    이메일의 일부분으로 고객 페이지 검색

--- src/main/java/com/bumsoap/store/repository/CustomerRepoI.java
+++ src/main/java/com/bumsoap/store/repository/CustomerRepoI.java
@@ -5,10 +5,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepoI extends JpaRepository<Customer, Long> {
-    @Query("SELECT c FROM Customer c")
-    Page<Customer> getOnePage(Pageable pageable);
+    @Query("""
+             SELECT c FROM Customer c
+              WHERE LOWER(c.email) LIKE LOWER(CONCAT('%', :email, '%'))
+            """)
+    Page<Customer> getOnePage(
+            @Param("email") String email, Pageable pageable);
 }
