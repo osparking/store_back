06dac820f7254fbe132e6957e8395996bc6748c1
BumTiger <jbpark03@gmail.com>
2026-07-08 10:25:40 +0900

    비번리셋 자격검증 토큰발급유저 검색 메소드 시험

--- src/test/java/com/bumsoap/store/service/token/VerifinTokenServTest.java
+++ src/test/java/com/bumsoap/store/service/token/VerifinTokenServTest.java
@@ -1,4 +1,47 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.bumsoap.store.service.token;
+
+import com.bumsoap.store.exception.DataNotFoundException;
+import com.bumsoap.store.model.BsUser;
+import com.bumsoap.store.repository.VerifinTokenRepoI;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
 class VerifinTokenServTest {
-  
+    String token = "08f15030-2ce1-47ab-98b0-c2de6dffac8b";
+
+    @Autowired
+    private VerifinTokenRepoI verifinTokenRepo;
+    @Autowired
+    private VerifinTokenServInt verifinTokenServ;
+
+    @Test
+    void findUserByGoodToken() {
+        Optional<BsUser> user =
+                verifinTokenRepo.findUserByVerificationToken(token);
+
+        Assertions.assertTrue(user.isPresent());
+    }
+
+    @Test
+    void findUserByWrongToken() {
+        Optional<BsUser> user =
+                verifinTokenRepo.findUserByVerificationToken("wrongtoken");
+
+        Assertions.assertTrue(user.isEmpty());
+    }
+
+    @Test
+    void findUserByToken() {
+        assertThrows(DataNotFoundException.class, () -> {
+            verifinTokenServ.findUserByToken(token + "?");
+        });
+    }
+
 }
