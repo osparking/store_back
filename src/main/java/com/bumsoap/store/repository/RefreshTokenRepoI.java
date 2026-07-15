42cd3e3020b794d3dac26a3a74743e6d0a8d3d15
BumTiger <jbpark03@gmail.com>
2026-07-15 15:52:36 +0900

    저장소-날 리프레시 토큰으로 RefreshToken 찾기

--- src/main/java/com/bumsoap/store/repository/RefreshTokenRepoI.java
+++ src/main/java/com/bumsoap/store/repository/RefreshTokenRepoI.java
@@ -3,5 +3,8 @@ package com.bumsoap.store.repository;
 import com.bumsoap.store.model.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepoI extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByTokenHash(String hashedToken);
 }
