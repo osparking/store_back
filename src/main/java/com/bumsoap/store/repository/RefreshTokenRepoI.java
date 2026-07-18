9f927f5dff7174f9486785dc7355d2ccac247e3e
BumTiger <jbpark03@gmail.com>
2026-07-18 16:01:03 +0900

    만료 리프레시 토큰 제거 저장소 메소드 원형

--- src/main/java/com/bumsoap/store/repository/RefreshTokenRepoI.java
+++ src/main/java/com/bumsoap/store/repository/RefreshTokenRepoI.java
@@ -3,8 +3,10 @@ package com.bumsoap.store.repository;
 import com.bumsoap.store.model.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface RefreshTokenRepoI extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByTokenHash(String hashedToken);
+    int deleteByExpiryDateBefore(LocalDateTime time);
 }
