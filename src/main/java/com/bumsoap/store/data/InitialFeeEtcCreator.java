2b1ea989ccf38baed262aa03ebccbe030aab5695
BumTiger <jbpark03@gmail.com>
2026-07-23 17:54:38 +0900

    비누상자 중량 및 권역별 초기 요금 저장

--- src/main/java/com/bumsoap/store/data/InitialFeeEtcCreator.java
+++ src/main/java/com/bumsoap/store/data/InitialFeeEtcCreator.java
@@ -2,6 +2,7 @@ package com.bumsoap.store.data;
 
 import com.bumsoap.store.model.FeeEtc;
 import com.bumsoap.store.repository.FeeEtcRepo;
+import com.bumsoap.store.util.BoxSize;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
@@ -12,27 +13,33 @@ import java.math.BigDecimal;
 @Component
 @RequiredArgsConstructor
 public class InitialFeeEtcCreator implements ApplicationListener<ApplicationReadyEvent> {
-  private final FeeEtcRepo feeEtcRepo;
+    private final FeeEtcRepo feeEtcRepo;
 
-  @Override
-  public void onApplicationEvent(ApplicationReadyEvent event) {
-    insertFeeEtcIfNotExists();
-  }
-
-  private void insertFeeEtcIfNotExists() {
-    // Check if there's no existing inventory for delivery fee, etc.
-    if (feeEtcRepo.count() == 0) {
-      // Create initial record of delivery fee and other value.
-      FeeEtc feeEtc = new FeeEtc(
-          BigDecimal.valueOf(3400),
-          BigDecimal.valueOf(3000),
-          BigDecimal.valueOf(5000),
-          BigDecimal.valueOf(40000)
-      );
-
-      // Save the new SoapInven object to the inventory
-      feeEtcRepo.save(feeEtc);
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        insertFeeEtcIfNotExists();
     }
-    System.out.println("기본 배송비 등 다양한 요금/기준 삽입됨");
-  }
+
+    private void insertFeeEtcIfNotExists() {
+        // Check if there's no existing inventory for delivery fee, etc.
+        if (feeEtcRepo.count()==0) {
+            // Create initial record of delivery fee and other value.
+            feeEtcRepo.save(new FeeEtc(BoxSize.BOX_03,
+                    BigDecimal.valueOf(4100),
+                    BigDecimal.valueOf(4600),
+                    BigDecimal.valueOf(7600),
+                    BigDecimal.valueOf(4000),
+                    BigDecimal.valueOf(40000)
+            ));
+
+            feeEtcRepo.save(new FeeEtc(BoxSize.BOX_12,
+                    BigDecimal.valueOf(5100),
+                    BigDecimal.valueOf(5600),
+                    BigDecimal.valueOf(8600),
+                    BigDecimal.ZERO,
+                    BigDecimal.ZERO
+            ));
+        }
+        System.out.println("비누 수량 및 수신처 지역별 배송비 등 삽입됨");
+    }
 }
