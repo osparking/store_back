c4b79d4343620714885e146efc78d3dd992d918b
BumTiger <jbpark03@gmail.com>
2026-07-24 09:54:07 +0900

    배송비, 무배 기준 등  초기 자료 입력 클래스 개정

--- src/main/java/com/bumsoap/store/data/InitialFeeEtcCreator.java
+++ src/main/java/com/bumsoap/store/data/InitialFeeEtcCreator.java
@@ -1,7 +1,9 @@
 package com.bumsoap.store.data;
 
 import com.bumsoap.store.model.FeeDelivery;
-import com.bumsoap.store.repository.FeeEtcRepo;
+import com.bumsoap.store.model.FeeOther;
+import com.bumsoap.store.repository.FeeDeliveryRepo;
+import com.bumsoap.store.repository.FeeOtherRepo;
 import com.bumsoap.store.util.BoxSize;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -13,11 +15,13 @@ import java.math.BigDecimal;
 @Component
 @RequiredArgsConstructor
 public class InitialFeeEtcCreator implements ApplicationListener<ApplicationReadyEvent> {
-    private final FeeEtcRepo feeEtcRepo;
+    private final FeeDeliveryRepo feeEtcRepo;
+    private final FeeOtherRepo feeOtherRepo;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
         insertFeeEtcIfNotExists();
+        insertFeeOtherIfNotExists();
     }
 
     private void insertFeeEtcIfNotExists() {
@@ -27,19 +31,27 @@ public class InitialFeeEtcCreator implements ApplicationListener<ApplicationRead
             feeEtcRepo.save(new FeeDelivery(BoxSize.BOX_03,
                     BigDecimal.valueOf(4100),
                     BigDecimal.valueOf(4600),
-                    BigDecimal.valueOf(7600),
-                    BigDecimal.valueOf(4000),
-                    BigDecimal.valueOf(40000)
+                    BigDecimal.valueOf(7600)
             ));
 
             feeEtcRepo.save(new FeeDelivery(BoxSize.BOX_12,
                     BigDecimal.valueOf(5100),
                     BigDecimal.valueOf(5600),
-                    BigDecimal.valueOf(8600),
-                    BigDecimal.ZERO,
-                    BigDecimal.ZERO
+                    BigDecimal.valueOf(8600)
             ));
         }
         System.out.println("비누 수량 및 수신처 지역별 배송비 등 삽입됨");
+    }
+
+    private void insertFeeOtherIfNotExists() {
+        // Check if there's no existing inventory for delivery fee, etc.
+        if (feeOtherRepo.count()==0) {
+            // 섬 배송비 및 무료 배송 기준 비누 구매 최소액
+            feeOtherRepo.save(new FeeOther(
+                    BigDecimal.valueOf(4000),
+                    BigDecimal.valueOf(40000)
+            ));
+        }
+        System.out.println("제주 외 섬 배송비 등 삽입됨");
     }
 }
