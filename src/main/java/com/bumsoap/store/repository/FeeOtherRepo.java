f154c1605af8da500c4af5e2d5e5185fd8fad38c
BumTiger <jbpark03@gmail.com>
2026-07-24 10:14:40 +0900

    JPQL 질의문 오류 수정 - 마리아DB 질의와 상이

--- src/main/java/com/bumsoap/store/repository/FeeOtherRepo.java
+++ src/main/java/com/bumsoap/store/repository/FeeOtherRepo.java
@@ -1,12 +1,10 @@
 package com.bumsoap.store.repository;
 
 import com.bumsoap.store.model.FeeOther;
-import com.bumsoap.store.util.BoxSize;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface FeeOtherRepo extends JpaRepository<FeeOther, Long> {
-    @Query("select fo.* from FeeOther fo order by fo.id desc limit 1")
-    FeeOther findLatestFeeOther(@Param("boxSize") BoxSize boxSize);
+    @Query("select fo from FeeOther fo order by fo.id desc limit 1")
+    FeeOther findLatestFeeOther();
 }
