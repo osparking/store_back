400a84f6a1b1ad127d5135e3828924e1dfa8268e
BumTiger <jbpark03@gmail.com>
2026-07-27 14:02:14 +0900

    댓글없는 질문 삭제 서비스 메소드 시험 메소드

--- src/test/java/com/bumsoap/store/service/question/QuestionServTest.java
+++ src/test/java/com/bumsoap/store/service/question/QuestionServTest.java
@@ -1,6 +1,7 @@
 package com.bumsoap.store.service.question;
 
 import com.bumsoap.store.repository.QuestionRepo;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,5 +27,13 @@ class QuestionServTest {
         var writerName = questionServ.findWriterNameByQuestionId(1L);
         System.out.println(writerName);
         assert (!writerName.isEmpty());
+    }
+
+    @Test
+    void deleteQuestion() {
+        Long questionId = 12L;
+        boolean deleted = questionServ.deleteQuestion(
+                questionId, "jbpark03@gmail.com");
+        Assertions.assertTrue(deleted);
     }
 }
