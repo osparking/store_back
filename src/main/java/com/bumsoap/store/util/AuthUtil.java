2fb6e89696aabb90f84b85b18c89cf133113f562
BumTiger <jbpark03@gmail.com>
2026-08-04 13:54:13 +0900

    환경변수 사용 메소드 빈 메소드로 재 정의

--- src/main/java/com/bumsoap/store/util/AuthUtil.java
+++ src/main/java/com/bumsoap/store/util/AuthUtil.java
@@ -5,6 +5,8 @@ import com.bumsoap.store.repository.UserRepoI;
 import com.bumsoap.store.security.user.BsUserDetails;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.stereotype.Component;
@@ -54,5 +56,25 @@ public class AuthUtil {
       return null;
     }
     return header.substring(7);
+  }
+
+  @Value("${auth.refresh.expirationSec}")
+  private int expirationSec;
+  /**
+   * (JWT) 토큰 리프레시 작업용 리프레시 토큰을 ResponseCookie 로 만든다.
+   * @param refreshToken 리스레시 토큰     *
+   * @return 생성된 ResponseCookie
+   */
+  public ResponseCookie createSaveCookie(String refreshToken) {
+    return ResponseCookie.from("refreshToken", refreshToken) // 키 이름
+            .httpOnly(true) // JavaScript 접근 차단 (보안 핵심)
+            // HTTPS 에서만 전송 (운영 환경 필수, 테스트 시 false 가능)
+            .secure(true)
+            // 모든 경로에서 쿠키 전송 (refresh 엔드포인트가
+            // - /autho/refresh_token 이므로 최소한 해당 경로 포함)
+            .path("/")
+            .maxAge(expirationSec) // (현장용) 1 주 / (시험용) 1 분
+            .sameSite("Strict") // CSRF 방지 (Strict 또는 Lax )
+            .build();
   }
 }
