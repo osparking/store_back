9c1b3ae7f35e8323efc1719d0af82bea13653ce8
BumTiger <jbpark03@gmail.com>
2026-08-17 14:25:08 +0900

    RT 부재/만료 상황 정상처리용-NoRefTokenFoundException

---
+++ src/main/java/com/bumsoap/store/exception/NoRefTokenFoundException.java
@@ -0,0 +1,7 @@
+package com.bumsoap.store.exception;
+
+public class NoRefTokenFoundException extends RuntimeException {
+    public NoRefTokenFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/bumsoap/store/exception/RefreshTokenException.java
+++
@@ -1,7 +0,0 @@
-package com.bumsoap.store.exception;
-
-public class RefreshTokenException extends RuntimeException {
-    public RefreshTokenException(String message) {
-        super(message);
-    }
-}
