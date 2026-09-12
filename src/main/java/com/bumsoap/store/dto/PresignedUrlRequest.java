6fc94ad645c4040162cbdda26372b702bb036238
BumTiger <jbpark03@gmail.com>
2026-09-12 16:05:46 +0900

    선서명 URL 요청 객체 domain 멤버 추가

--- src/main/java/com/bumsoap/store/dto/PresignedUrlRequest.java
+++ src/main/java/com/bumsoap/store/dto/PresignedUrlRequest.java
@@ -3,6 +3,8 @@ package com.bumsoap.store.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record PresignedUrlRequest(
+        @NotBlank String domain,
+
         @NotBlank(message = "파일명은 필수입니다.")
         String fileName,
 
