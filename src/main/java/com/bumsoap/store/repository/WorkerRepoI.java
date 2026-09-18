18478ad1955951ce12e759bd7169cfee26935884
BumTiger <jbpark03@gmail.com>
2026-09-18 10:19:02 +0900

    관리자의 직원 관리 페이지 채취 저장소 메소드

--- src/main/java/com/bumsoap/store/repository/WorkerRepoI.java
+++ src/main/java/com/bumsoap/store/repository/WorkerRepoI.java
@@ -3,6 +3,8 @@ package com.bumsoap.store.repository;
 import com.bumsoap.store.dto.PeopleByDept;
 import com.bumsoap.store.model.Worker;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -46,4 +48,6 @@ public interface WorkerRepoI extends JpaRepository<Worker, Long> {
                     WHERE bu.email = :email
                     """)
     Optional<Boolean> isAccountDeleted(@Param("email") String email);
+
+    Page<Worker> getWorkersByDept(String dept, Pageable pageable);
 }
